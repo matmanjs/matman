b5204c181baa8421e1bccfa70c484faf58e0e21f
helinjiang <me@helinjiang.com>
2017-05-07 17:03:24 +0800

    refactor: 重构了require-file.js的测试用例

--- test/scripts/util/require-file.test.js
+++ test/scripts/util/require-file.test.js
@@ -2,11 +2,19 @@ import path from 'path';
 import fse from 'fs-extra';
 import { expect } from 'chai';
 
-import requireFile from '../../mat/util/require-file';
+import requireFile from '../../../mat/util/require-file';
 
-const BASE_PATH_SRC = path.join(__dirname, '../data/fixtures');
-const BASE_PATH_TMP = path.join(__dirname, '../tmp');
-const FOLDER_NAME = 'require-file';
+// 测试目标
+const TEST_TARGET = 'util/require-file.js';
+
+const ROOT_PROJECT = path.join(__dirname, '../../../');
+const ROOT_TEST = path.join(ROOT_PROJECT, './test');
+
+const BASE_PATH_SRC = path.join(ROOT_TEST, './data/fixtures', path.dirname(TEST_TARGET));
+const BASE_PATH_TMP = path.join(ROOT_TEST, './tmp', path.dirname(TEST_TARGET));
+
+// 以测试文件名为目录，将测试用的素材存储其中
+const FOLDER_NAME = path.basename(TEST_TARGET, '.js');
 
 describe('require-file.js', () => {
     const TMP_SAVE_FOLDER = path.join(BASE_PATH_TMP, FOLDER_NAME);
