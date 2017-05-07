b48b2311680057c8b4329e98973045d5e6480209
helinjiang <me@helinjiang.com>
2017-05-07 14:21:21 +0800

    feat: 使用 es6 语法来写测试用例

--- test/scripts/file-save.test.js
+++ test/scripts/file-save.test.js
@@ -1,38 +1,36 @@
-var fse = require('fs-extra');
-var Promise = require('bluebird');
-var fileSave = require('../../mat/util/file-save');
+import fse from 'fs-extra';
+import { expect } from 'chai';
 
-var expect = require('chai').expect;
+import fileSave from '../../mat/util/file-save';
 
-describe('util/file-save.js saveJson()', function () {
-    before(function (done) {
+describe('util/file-save.js saveJson()', () => {
+    before((done) => {
         // 删除临时文件目录
         fse.removeSync('./test/tmp/util-file-save');
 
         fileSave.saveJson(require('../data/fixtures/util-file-save/file.js'), './test/tmp/util-file-save/file.json')
-            .then(function () {
+            .then(() => {
                 done();
             })
-            .catch(function (err) {
-                console.error('err', err);
+            .catch((err) => {
+                // console.error('err', err);
                 done();
             });
     });
 
-    after(function () {
+    after(() => {
         fse.removeSync('./test/tmp/util-file-save');
     });
 
-    it('Save json object to .json file success!', function (done) {
+    it('Save json object to .json file success!', (done) => {
         // 保存的文件内容
-        var newSavedFile = fse.readFileSync('./test/tmp/util-file-save/file.json', 'utf8');
+        const newSavedFile = fse.readFileSync('./test/tmp/util-file-save/file.json', 'utf8');
 
         // 预期的文件内容
-        var expectedFile = fse.readFileSync('./test/data/expected/util-file-save/file.json', 'utf8');
+        const expectedFile = fse.readFileSync('./test/data/expected/util-file-save/file.json', 'utf8');
 
         expect(newSavedFile).to.equal(expectedFile);
 
         done();
     });
-
 });
