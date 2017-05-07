71e58dbe619f8031e7f3ce0e3c3835184ea80c3c
helinjiang <me@helinjiang.com>
2017-05-07 15:27:04 +0800

    refactor: 重构了测试用例的写法，将变量提出来

--- test/scripts/file-save.test.js
+++ test/scripts/file-save.test.js
@@ -7,13 +7,15 @@ const BASE_PATH_TMP = './test/tmp';
 const BASE_PATH_EXPECTED = './test/data/expected';
 const BASE_PATH_SRC = '../data/fixtures';
 const FOLDER_NAME = 'util-file-save';
+const FILE_SUFFIX = '.json';
 
 describe('util/file-save.js saveJson()', () => {
-    const TMP_SAVE_FOLDER = path.join(BASE_PATH_TMP, FOLDER_NAME);
-    const TMP_SAVE_FILE = path.join(TMP_SAVE_FOLDER, 'file.json');
-
-    const EXPECTED_SAVE_FILE = path.join(BASE_PATH_EXPECTED, FOLDER_NAME, 'file.json');
     const SRC_FILE = path.join(BASE_PATH_SRC, FOLDER_NAME, 'file.js');
+    const OUTPUT_FILE_NAME = path.basename(SRC_FILE, '.js') + FILE_SUFFIX;
+
+    const TMP_SAVE_FOLDER = path.join(BASE_PATH_TMP, FOLDER_NAME);
+    const TMP_SAVE_FILE = path.join(TMP_SAVE_FOLDER, OUTPUT_FILE_NAME);
+    const EXPECTED_SAVE_FILE = path.join(BASE_PATH_EXPECTED, FOLDER_NAME, OUTPUT_FILE_NAME);
 
     before((done) => {
         // 删除临时文件目录
