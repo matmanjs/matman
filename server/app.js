196450bed626913b967d6848c50ba7d1cd21a965
helinjiang <me@helinjiang.com>
2017-05-08 12:51:41 +0800

    feat: 修改express，新增cgi路由单独处理服务器的接口

--- server/app.js
+++ server/app.js
@@ -1,13 +1,55 @@
 var express = require('express');
 var path = require('path');
+
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+var admin = require('./routes/admin');
+var cgi = require('./routes/cgi');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.use('/admin', admin);
+app.use('/cgi', cgi);
+
+// 将所有未定义的页面都指向index.html中，包括首页
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
-app.listen(9000, function () {
-    console.log('MATMAN listening on port 9000!')
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
+
+// app.listen(9000, function () {
+//     console.log('MATMAN listening on port 9000!')
+// });
---
+++ server/routes/cgi.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET cgi listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+module.exports = router;
