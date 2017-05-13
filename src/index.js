2cffc29e54127bacde9ad6ebf434bc9587e3198b
helinjiang <me@helinjiang.com>
2017-05-13 17:16:03 +0800

    refactor: 删除无用的npm包，增加一些文件使得提交代码更规范

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
-var path = require('path');
-
-var fse = require('./util/fse');
+var version = require('./MatmanVersion');
 var mockerModuleTool = require('./mocker/mocker-module-tool');
 
-const ROOT_PROJECT = path.join(__dirname, './');
-
-var targetPath = './test/purejson.js';
-
-var result = mockerModuleTool.getResult(targetPath);
-
-console.log(result);
-
-mockerModuleTool.save(targetPath, './test/output/r.json')
-    .then((data) => {
-        console.log('then', data);
-    })
-    .catch((err) => {
-        console.log('catch', err);
-    })
-
-
+module.exports = {
+    mocker: {
+        tool: mockerModuleTool
+    },
+    version: version
+};
