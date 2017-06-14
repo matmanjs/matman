665722ca3300e59e20e2ea325f0bc7cd073883ca
helinjiang <me@helinjiang.com>
2017-06-14 13:35:30 +0800

    feat: 新增/id/:id 路由

--- src/server/router-mocker/index.js
+++ src/server/router-mocker/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const methodOverride = require('method-override');
 const _ = require('lodash');
 const bodyParser = require('../body-parser');
-const mixins = require('../mixins');
 
 module.exports = (source) => {
   // Create router
@@ -12,24 +11,26 @@ module.exports = (source) => {
   router.use(methodOverride());
   router.use(bodyParser);
 
-  // Expose database
-  router.db = { name: 'mocker' };
-
   // Expose render
   router.render = (req, res) => {
     res.jsonp(res.locals.data)
   };
 
   // GET /list
-  router.use('/list', (req, res) => {
+  router.get('/list', (req, res) => {
     res.jsonp({ name: 'list' });
   });
 
+  // GET /id/:id
+  router.get('/id/:id', function (req, res) {
+    res.jsonp({ id: req.params.id });
+  });
+
   router.use((req, res) => {
-    // if (!res.locals.data) {
-    //   res.status(404);
-    //   res.locals.data = {};
-    // }
+    if (!res.locals.data) {
+      res.status(404);
+      res.locals.data = {};
+    }
 
     router.render(req, res);
   });
