1a51957d0257b655ee34ea8dfa47aae1c62a1168
helinjiang <me@helinjiang.com>
2017-06-14 20:20:17 +0800

    feat: 新增 mock-server

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 module.exports = {
   create: () => express().set('json spaces', 2),
   defaults: require('./defaults'),
+  mockServer: require('./mock-server'),
   router: require('./router'),
   routerMocker: require('./router-mocker'),
   rewriter: require('./rewriter'),
