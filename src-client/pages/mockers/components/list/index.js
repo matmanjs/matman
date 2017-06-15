e65c40793c5bc58d92d8f4e4e95bc40ebf154781
helinjiang <me@helinjiang.com>
2017-06-15 16:05:46 +0800

    refactor: mockers list 模块使用 React 标准方式重写

--- src-client/pages/mockers/components/list/index.js
+++ src-client/pages/mockers/components/list/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
 
-const MockerList = ({ match }) => (
-  <div>
-    <h3>hhhhhhhh</h3>
-    <h3>{JSON.stringify(match)}</h3>
-  </div>
-);
+class MockerList extends Component {
+  constructor(props, context) {
+    super(props, context);
 
+  }
+
+  componentDidMount() {
+    console.log('MockerList componentDidMount', this.props);
+    // this.props.match === {"path":"/admin/mockers","url":"/admin/mockers","isExact":true,"params":{}}
+  }
+
+  render() {
+    const { match } = this.props;
+
+    return (
+      <div>
+        <h3>hhhhhhhh</h3>
+        <h3>{JSON.stringify(match)}</h3>
+      </div>
+    )
+  }
+}
+
+// function mapStateToProps(state) {
+//   const {} = state;
+//
+//   return {}
+// }
+//
+// function mapDispatchToProps(dispatch) {
+//   return {};
+// }
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(MockerList);
 export default MockerList;
+
