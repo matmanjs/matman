31722da94f6b893b345300b24d36f2b64d5554cb
helinjiang <me@helinjiang.com>
2017-06-21 11:35:06 +0800

    refactor: 将原来的 /* 方式路由修改为为每个 mocker 独立路由，以便后续支持自定义路由，增加 post 请求的支持

--- src/server/router-mocker/index.js
+++ src/server/router-mocker/index.js
@@ -8,6 +8,8 @@ const business = require('../business');
 module.exports = (entryPath) => {
   const entry = require(entryPath);
 
+  let mockerList = business.getMockerList(entry.MOCKER_PATH);
+
   // Create router
   const router = express.Router();
 
@@ -22,9 +24,9 @@ module.exports = (entryPath) => {
 
   // GET /sys-cgi/mocker 所有的 mocker 列表信息
   router.get('/sys-cgi/mocker', (req, res) => {
-    let result = business.getMockerList(entry.MOCKER_PATH);
+    mockerList = business.getMockerList(entry.MOCKER_PATH);
 
-    res.jsonp(result);
+    res.jsonp(mockerList);
   });
 
   // GET /sys-cgi/mocker/:mockerName 获得这个 mocker 的信息
@@ -41,53 +43,113 @@ module.exports = (entryPath) => {
     res.jsonp(result);
   });
 
-  // GET /*
-  router.get('/*', function (req, res) {
-    // Express的req对象，详见 http://expressjs.com/en/4x/api.html#req
-    // 例如：/test/two/?t=1，
-    // req.query.t=1
-    // req.params[0]="test/two"
+  // 根据用户配置的路由关系，进行解析
+  // console.log('mockerList', mockerList);
+  mockerList.forEach((mockerData) => {
+    //TODO cgi 可能不是以 / 开头的，建议以 route 形式会更好
 
-    let mockerBasePath = entry.MOCKER_PATH
-    let url = req.params[0];
-    let params = req.query;
+    let type = mockerData.type || 'get';
 
-    business.getMockModule(mockerBasePath, url, params)
-      .then((result) => {
-        // res.jsonp({
-        //   url: req.url,
-        //   params: req.params,
-        //   query: req.query,
-        //   data: result
-        // });
-        res.jsonp(result);
-      })
-      .catch((err) => {
-        // 注意 err 有可能是 Error 对象，也可能是普通的字符串或对象
-        let errMsg = err.stack || err;
+    router[type](mockerData.cgi, function (req, res) {
+      // Express的req对象，详见 http://expressjs.com/en/4x/api.html#req
+      // 例如：/test/two/?t=1，
+      // req.query.t=1
+      // req.params[0]="test/two"
 
-        console.error(errMsg);
-        // 如果是未知的CGI，则透传结果即可
-        if (errMsg === 'UNKNOWN_CGI') {
-          // Load remote data
-          const opts = {
-            url: 'http://' + req.headers.host + req.url,
-            json: true
-          };
+      // post 请求
+      // mockerData.cgi="/cgi-bin/a/b/post_cgi"
+      // post http://localhost:3000/cgi-bin/a/b/post_cgi data={activeModule:"error_not_login"}
+      // req.baseUrl=""
+      // req.originalUrl="/cgi-bin/a/b/post_cgi"
+      // req.url="/cgi-bin/a/b/post_cgi"
+      // req.method="POST"
+      // req.OriginalMethod="POST"
+      // req.body.activeModule = "error_not_login"
+      // req.body = data
 
-          request(opts, (err, response) => {
-            if (err) {
-              res.status(500).send(err);
-            } else {
-              res.jsonp(response.body);
-            }
-          })
-        } else {
-          res.status(500).send(errMsg);
-        }
+      // get 请求
+      // mockerData.cgi="/cgi-bin/a/b/simple_cgi"
+      // get http://localhost:3000/cgi-bin/a/b/simple_cgi?activeModule=error_not_login
+      // req.baseUrl=""
+      // req.originalUrl="/cgi-bin/a/b/simple_cgi?activeModule=error_not_login"
+      // req.url="/cgi-bin/a/b/simple_cgi?activeModule=error_not_login"
+      // req.method="GET"
+      // req.OriginalMethod="GET"
+      // req.query.activeModule = "error_not_login"
 
-      });
+      let mockerBasePath = entry.MOCKER_PATH;
+      // let url = req.params[0];
+      let url = mockerData.cgi;
+      let params = (type === 'post') ? req.body : req.query;
+
+      console.log('router type 3', type, mockerData.cgi, url, params)
+
+      business.getMockModule(mockerBasePath, url, params)
+        .then((result) => {
+          // res.jsonp({
+          //   url: req.url,
+          //   params: req.params,
+          //   query: req.query,
+          //   data: result
+          // });
+          res.jsonp(result);
+        })
+        .catch((err) => {
+          // 注意 err 有可能是 Error 对象，也可能是普通的字符串或对象
+          let errMsg = err.stack || err;
+
+          console.error(errMsg);
+        });
+    });
   });
+
+  // GET /*
+  // router.get('/*', function (req, res) {
+  //   // Express的req对象，详见 http://expressjs.com/en/4x/api.html#req
+  //   // 例如：/test/two/?t=1，
+  //   // req.query.t=1
+  //   // req.params[0]="test/two"
+  //
+  //   let mockerBasePath = entry.MOCKER_PATH
+  //   let url = req.params[0];
+  //   let params = req.query;
+  //
+  //   business.getMockModule(mockerBasePath, url, params)
+  //     .then((result) => {
+  //       // res.jsonp({
+  //       //   url: req.url,
+  //       //   params: req.params,
+  //       //   query: req.query,
+  //       //   data: result
+  //       // });
+  //       res.jsonp(result);
+  //     })
+  //     .catch((err) => {
+  //       // 注意 err 有可能是 Error 对象，也可能是普通的字符串或对象
+  //       let errMsg = err.stack || err;
+  //
+  //       console.error(errMsg);
+  //       // 如果是未知的CGI，则透传结果即可
+  //       if (errMsg === 'UNKNOWN_CGI') {
+  //         // Load remote data
+  //         const opts = {
+  //           url: 'http://' + req.headers.host + req.url,
+  //           json: true
+  //         };
+  //
+  //         request(opts, (err, response) => {
+  //           if (err) {
+  //             res.status(500).send(err);
+  //           } else {
+  //             res.jsonp(response.body);
+  //           }
+  //         })
+  //       } else {
+  //         res.status(500).send(errMsg);
+  //       }
+  //
+  //     });
+  // });
 
   router.use((req, res) => {
     if (!res.locals.data) {
