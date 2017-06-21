886e55a8bb2af4bff8c16cafb8fc23cd29fa7c83
helinjiang <me@helinjiang.com>
2017-06-21 17:58:47 +0800

    feat: 支持 id/:id/:subId 这样的复杂路由，并将参数透传给 mock module，以便用户自行处理

--- test/demo/src/mocker/param_in_path/mock_modules/success_simple/index.js
+++ test/demo/src/mocker/param_in_path/mock_modules/success_simple/index.js
@@ -1,17 +1,10 @@
-// export default function getResult(param, req) {
-//   console.log('param', param);
-//   console.log('req', req);
-//
-//   return {
-//     "id": -1,
-//   }
-// }
-
 module.exports = (params, req) => {
-  console.log('params', params);
+  // console.log('params', params);
   // console.log('req', req);
 
   return {
     "id": params.id,
+    "subId": params.subId,
+    "url": req.url,
   }
 };
