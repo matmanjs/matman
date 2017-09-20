397a8eaecd18e6802addd63013a129d6665a60cf
helinjiang <19870201_hlj>
2017-09-20 16:14:13 +0800

    feat: 新增 getMixinHandleModuleData 方法，并增加相应的测试用例

--- src/parser/parser-util.js
+++ src/parser/parser-util.js
@@ -5,7 +5,7 @@ const path = require('path');
 /**
  * 获取 handlerData 信息
  *
- * @param {String} handlerName
+ * @param {String} handlerName 该 handler 名字
  * @param {Object} handlerConfigDBState config.json中的数据
  * @param {Object} cacheData 缓存数据
  * @return {Object}
@@ -36,6 +36,29 @@ function getMixinHandlerData(handlerName, handlerConfigDBState, cacheData) {
   return data;
 }
 
+/**
+ * 获取 handle_module 信息
+ *
+ * @param {String} handleModuleName 该 handle_module 名字
+ * @param {Object} handleModuleConfigDBState config.json中的数据
+ * @return {Object}
+ */
+function getMixinHandleModuleData(handleModuleName, handleModuleConfigDBState) {
+
+  // 始终以 config.json 中定义的为最高优先级（除了defaultModule字段），如果没定义，则使用 cache 值，最后才是默认值
+  let data = _.merge({
+    name: handleModuleName,
+    description: handleModuleName,
+    priority: 0,
+  }, handleModuleConfigDBState);
+
+  // TODO 如果是 /id/:id 类型的，则此处可能会有问题，或许还需要把请求值放入到query中
+  data.query = _.merge({}, data.query, { _m_target: handleModuleName });
+
+  return data;
+}
+
 module.exports = {
-  getMixinHandlerData: getMixinHandlerData
+  getMixinHandlerData: getMixinHandlerData,
+  getMixinHandleModuleData: getMixinHandleModuleData,
 };
--- test/scripts/parser/parser-util.test.js
+++ test/scripts/parser/parser-util.test.js
@@ -3,9 +3,9 @@ const expect = chai.expect;
 
 const parserUtil = require('../../../src/parser/parser-util');
 
-describe('parser-util.js', () => {
+describe('parser-util.js getMixinHandlerData()', () => {
 
-  describe('getMixinHandlerData() not define property of route', () => {
+  describe('not define property of route', () => {
     let handlerInfo;
 
     before(() => {
@@ -17,7 +17,7 @@ describe('parser-util.js', () => {
     });
   });
 
-  describe('getMixinHandlerData() default value', () => {
+  describe('default value', () => {
     let handlerInfo;
 
     before(() => {
@@ -46,7 +46,7 @@ describe('parser-util.js', () => {
     });
   });
 
-  describe('getMixinHandlerData() special value', () => {
+  describe('special value', () => {
     let handlerInfo;
 
     before(() => {
@@ -83,7 +83,7 @@ describe('parser-util.js', () => {
     });
   });
 
-  describe('getMixinHandlerData() have cache data', () => {
+  describe('have cache data', () => {
     let handlerInfo;
 
     before(() => {
@@ -122,6 +122,65 @@ describe('parser-util.js', () => {
 
     it('should contain property of tags', () => {
       expect(handlerInfo.tags).to.include.members(['全部', 'tag1', 'tag2', 'tag3']);
+    });
+  });
+
+});
+
+describe('parser-util.js getMixinHandleModuleData()', () => {
+  describe('default value', () => {
+    let handleModuleInfo;
+
+    before(() => {
+      handleModuleInfo = parserUtil.getMixinHandleModuleData('default_value', {});
+    });
+
+    it('should return object', () => {
+      expect(handleModuleInfo).to.be.an('object');
+    });
+
+    it('should contain default values', () => {
+      expect(handleModuleInfo).to.include({
+        name: 'default_value',
+        description: 'default_value',
+        priority: 0,
+      });
+    });
+
+    it('should contain property of query', () => {
+      expect(handleModuleInfo.query).to.include({
+        _m_target: 'default_value'
+      });
+    });
+  });
+
+  describe('special value', () => {
+    let handleModuleInfo;
+
+    before(() => {
+      handleModuleInfo = parserUtil.getMixinHandleModuleData('special_value', {
+        name: 'special_value',
+        description: 'special_value description',
+        priority: 99
+      });
+    });
+
+    it('should return object', () => {
+      expect(handleModuleInfo).to.be.an('object');
+    });
+
+    it('should contain special values', () => {
+      expect(handleModuleInfo).to.include({
+        name: 'special_value',
+        description: 'special_value description',
+        priority: 99
+      });
+    });
+
+    it('should contain property of query', () => {
+      expect(handleModuleInfo.query).to.include({
+        _m_target: 'special_value'
+      });
     });
   });
 
