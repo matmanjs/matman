8ab514d4acdb7f233f94707d38a4d5103b805d9c
helinjiang <19870201_hlj>
2017-09-22 11:56:43 +0800

    feat: 增加 demo_handle_modules 的用例测试

--- test/scripts/parser/handler-parser.test.js
+++ test/scripts/parser/handler-parser.test.js
@@ -14,10 +14,13 @@ describe('handler-parser.js getAllHandler()', () => {
   let allHandlerInfo;
 
   before(() => {
+    fse.removeSync(BASE_PATH_EXPECTED);
+
     let handlerParser = new HandlerParser(BASE_PATH_FIXTURES, BASE_PATH_EXPECTED);
 
     allHandlerInfo = handlerParser.getAllHandler();
     console.log(allHandlerInfo)
+
   });
 
   // after(() => {
@@ -62,6 +65,55 @@ describe('handler-parser.js getAllHandler()', () => {
 
     it('data.modules is correct', () => {
       expect(data.modules.map(item => item.name)).to.include.members(['error', 'success']);
+    });
+
+  });
+
+  describe('check demo_handle_modules', () => {
+    let data;
+
+    before(() => {
+      allHandlerInfo.forEach((item) => {
+        if (!data && (item.name === 'name_demo_handle_modules')) {
+          data = item;
+        }
+      })
+    });
+
+    it('should return an object', () => {
+      expect(data).to.be.an('object');
+    });
+
+    it('should have some properties', () => {
+      expect(data).to.include({
+        "name": "name_demo_handle_modules",
+        "description": "description_demo_handle_modules",
+        "disable": false,
+        "method": "get",
+        "priority": 0,
+        "route": "/cgi-bin/a/b/demo_handle_modules",
+        "activeModule": "success_1",
+        "defaultModule": "success_1",
+      });
+    });
+
+    it('data._fullPath include words of "demo_handle_modules"', () => {
+      // D:\\gitprojects\\matman\\test\\data\\fixtures\\parser\\demo_simple
+      expect(data._fullPath).to.include('demo_handle_modules');
+    });
+
+    it('data.modules length is 5', () => {
+      expect(data.modules).have.lengthOf(5);
+    });
+
+    it('data.modules is correct', () => {
+      expect(data.modules.map(item => item.name)).to.include.members([
+        'error',
+        'success_1',
+        'success_2',
+        'name_success_3',
+        'success_4'
+      ]);
     });
 
   });
