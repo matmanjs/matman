534e2cf6bdc789fb089d211a311426f8ec8d1014
helinjiang <me@helinjiang.com>
2017-10-31 18:31:24 +0800

    feat: 弹出结果框可以编辑之后再触发

--- src-client/pages/stubs/components/stub/display-show-result/index.js
+++ src-client/pages/stubs/components/stub/display-show-result/index.js
@@ -1,40 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Modal, Button } from 'antd';
 
 import './index.less';
 
-export default function StubShowResult(props) {
-  const { isShow, data, onHide, onEmitStub } = props;
+export default class StubShowResult extends Component {
+  constructor(props, context) {
+    super(props, context);
 
-  return (
-    <div className="stub-show-result">
+    this.state = {
+      curMsg: ''
+    };
 
-      <Modal
-        title="结果"
-        visible={isShow}
-        onCancel={onHide}
-        onOk={onHide}
-        footer={[
-          <Button key="stub" type="primary" size="large" onClick={onEmitStub.bind(this, data)}>
-            执行打桩操作
-          </Button>,
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEmitStub = this.handleEmitStub.bind(this);
+  }
 
-          <Button key="cancel" size="large" onClick={onHide}>
-            知道了
-          </Button>
-        ]}
-      >
+  componentDidMount() {
+    this.setState({
+      curMsg: JSON.stringify(this.props.data, null, 2)
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.data !== nextProps.data) {
+      this.setState({
+        curMsg: JSON.stringify(nextProps.data, null, 2)
+      });
+    }
+  }
+
+  handleChange(event) {
+    console.log('==handleChange==', typeof event.target.value, event.target.value);
+    this.setState({
+      curMsg: event.target.value
+    });
+  }
+
+  handleEmitStub() {
+    this.props.onEmitStub(this.state.curMsg);
+  }
+
+  render() {
+    const { curMsg } = this.state;
+    const { isShow, onHide } = this.props;
+    return (
+      <div className="stub-show-result">
+
+        <Modal
+          title="结果"
+          visible={isShow}
+          onCancel={onHide}
+          onOk={onHide}
+          footer={[
+            <Button key="stub" type="primary" size="large" onClick={this.handleEmitStub}>
+              执行打桩操作
+            </Button>,
+
+            <Button key="cancel" size="large" onClick={onHide}>
+              知道了
+            </Button>
+          ]}
+        >
 
         <textarea
-          name="cgidata"
-          id="cgidata"
           style={{ width: '100%', minHeight: '600px' }}
-          value={JSON.stringify(data, null, 2)}
-          readOnly
+          value={curMsg}
+          onChange={this.handleChange}
         />
 
-      </Modal>
+        </Modal>
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
