86b18998ea91ab5d8cbc67f969372379a24f0e88
helinjiang <me@helinjiang.com>
2018-04-10 15:04:58 +0800

    feat: 更新用例

---
+++ demo/02_mocker_advanced/app/handlers/user_info/handle_modules/error_not_login/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+exports.__esModule = true;
+exports.default = getResult;
+
+var _base = require('../../base');
+
+function getResult() {
+  return (0, _base.getFailData)(100000);
+}
