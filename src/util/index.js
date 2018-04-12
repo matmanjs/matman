b6d9d7abfaee3307b686353b80025162c5c57e1e
helinjiang <me@helinjiang.com>
2018-04-12 11:34:04 +0800

    feat: 引入 watch 文件的功能

--- src/util/index.js
+++ src/util/index.js
@@ -1,8 +1,12 @@
 const request = require('./request');
 const url = require('./url');
+const NodeWatchFileSystem = require('./NodeWatchFileSystem');
+const watch = require('./watch');
 
 module.exports = {
   requestInMock: request.requestInMock,
   query: url.query,
-  param: url.param
+  param: url.param,
+  NodeWatchFileSystem: NodeWatchFileSystem,
+  watch: watch
 };
