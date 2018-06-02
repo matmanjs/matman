d3fb0dfd291000d565eb493f14a8548b5f577538
linjianghe <linjianghe@tencent.com>
2018-06-02 16:54:40 +0800

    refactor: 获取模块的数据

--- src/mocker/MockModule.js
+++ src/mocker/MockModule.js
@@ -1,3 +1,5 @@
+const fsHandler = require('fs-handler');
+
 class MockModule {
     /**
      * 构造函数
@@ -10,6 +12,32 @@ class MockModule {
         this.name = name;
         this.module = module;
         this.config = config;
+    }
+
+    /**
+     * 获得该模块的最终结果
+     *
+     * @param {*} props
+     * @returns {Promise}
+     */
+    getResult(...props) {
+        return getTargetResult(this.module, ...props);
+    }
+}
+
+/**
+ * 获得模块内容之后，计算出最终返回值
+ *
+ * @param {*} saveTarget 有可能是函数、对象或者普通字符串
+ * @param {*} props saveTarget 为函数时，会透传给它
+ * @return {Promise}
+ */
+function getTargetResult(saveTarget, ...props) {
+    try {
+        let result = (typeof saveTarget === 'function') ? saveTarget(...props) : saveTarget;
+        return Promise.resolve(result);
+    } catch (e) {
+        return Promise.reject(e);
     }
 }
 
