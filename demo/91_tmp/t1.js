114c5fe724acb7d447634a483559805beaa4373e
linjianghe <linjianghe@tencent.com>
2018-06-02 17:36:21 +0800

    refactor: 新增 mock module config 的操作

--- demo/91_tmp/t1.js
+++ demo/91_tmp/t1.js
@@ -6,18 +6,18 @@ const mockerParser = new MockerParser({
     basePath: path.join(__dirname, './mock_service/mockers')
 });
 
-let mockerList = mockerParser.getAllMocker();
+// let mockerList = mockerParser.getAllMocker();
 
 // console.log(mockerList);
-// console.log(mockerParser.getMockerByName('demo_simple'));
-// console.log(mockerParser.getMockModuleByName('demo_simple', 'error'));
+console.log(mockerParser.getMockerByName('demo_simple'));
+console.log(mockerParser.getMockModuleByName('demo_simple', 'error'));
 
-console.log(mockerParser.getMockerByName('name_demo_basic'));
-
-console.log('\n');
-
-mockerParser.getMockModuleByName('name_demo_basic', 'success_4').getResult().then((data) => {
-    console.log(data);
-}).catch((err) => {
-    console.error(err);
-});
+// console.log(mockerParser.getMockerByName('name_demo_basic'));
+//
+// console.log('\n');
+//
+// mockerParser.getMockModuleByName('name_demo_basic', 'success_4').getResult().then((data) => {
+//     console.log(data);
+// }).catch((err) => {
+//     console.error(err);
+// });
--- src/mocker/MockModule.js
+++ src/mocker/MockModule.js
@@ -1,4 +1,4 @@
-const fsHandler = require('fs-handler');
+const MockModuleConfig = require('./MockModuleConfig');
 
 class MockModule {
     /**
@@ -8,10 +8,10 @@ class MockModule {
      * @param {Object | Function} module require('./index.js')
      * @param {Object} [config] require('./config.json')，有可能没有，此时则为null
      */
-    constructor(name, module, config = null) {
+    constructor(name, module, config) {
         this.name = name;
         this.module = module;
-        this.config = config;
+        this.config = new MockModuleConfig(name, config);
     }
 
     /**
