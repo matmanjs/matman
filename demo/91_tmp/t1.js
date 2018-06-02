4979e09c1e44d6ecd643714bacf272cf8a86b078
helinjiang <onehlj@qq.com>
2018-06-02 15:24:19 +0800

    refactor: 获取所有 mocker 名字

--- demo/91_tmp/t1.js
+++ demo/91_tmp/t1.js
@@ -5,3 +5,5 @@ const MockerParser = require('../../src/mocker/MockerParser');
 const mockerParser = new MockerParser({
     basePath: path.join(__dirname, './mock_service/mockers')
 });
+
+console.log(mockerParser.getAllMocker())
--- src/mocker/MockerParser.js
+++ src/mocker/MockerParser.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const fsHandler = require('fs-handler');
+
 class MockerParser {
     /**
      * 构造函数
@@ -8,7 +11,7 @@ class MockerParser {
      */
     constructor(opts) {
         this.basePath = opts.basePath;
-        this.matmanMockers = [...opts.matmanMockers];
+        this.matmanMockers = Array.isArray(opts.matmanMockers) ? [...opts.matmanMockers] : [];
     }
 
     /**
@@ -18,6 +21,21 @@ class MockerParser {
      * @return {Array}
      */
     getAllMocker(isReset) {
+        // 1. 获取所有的 handler
+        fsHandler.search.getAll(this.basePath, { globs: ['*'] }).forEach((item) => {
+            // 限制只处理文件夹类型的，不允许在 basePath 目录下有非文件夹的存在
+            if (!item.isDirectory()) {
+                console.error(`${path.join(item.basePath, item.relativePath)} SHOULD BE Directory!`);
+                return;
+            }
+
+            // 模块名字，默认取文件名，
+            // 在根目录下，每个子文件夹就是一个 mocker 单位，其名字即为文件夹名字
+            let name = path.basename(item.relativePath);
+
+            console.log('找到 mocker ：', name);
+
+        });
 
     }
 }
