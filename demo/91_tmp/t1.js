a50229f7c663f1265aa2462d34612a3930e7107a
helinjiang <onehlj@qq.com>
2018-06-02 15:13:49 +0800

    refactor: 修改样例

---
+++ demo/91_tmp/t1.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const MockerParser = require('../../src/mocker/MockerParser');
+
+const mockerParser = new MockerParser({
+    basePath: path.join(__dirname, './mock_service/mockers')
+});
--- src/mocker/MockerParser.js
+++ src/mocker/MockerParser.js
@@ -2,10 +2,12 @@ class MockerParser {
     /**
      * 构造函数
      *
-     * @param opts 参数
-     * @param opts.matmanMockers 参数
+     * @param {Object} opts 参数
+     * @param {String} opts.basePath mocker的根目录
+     * @param {Array} [opts.matmanMockers] MatmanMocker 列表
      */
     constructor(opts) {
+        this.basePath = opts.basePath;
         this.matmanMockers = [...opts.matmanMockers];
     }
 
