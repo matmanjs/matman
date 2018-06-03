168fe7b4aeea12b9ef09c07510553910cb58afe3
helinjiang <me@helinjiang.com>
2018-06-03 21:58:54 +0800

    refactor: MockModule 的测试用例

--- test/scripts/mocker/MockModule.test.js
+++ test/scripts/mocker/MockModule.test.js
@@ -31,27 +31,71 @@ describe('./mocker/MockModule', () => {
         });
     });
 
-    describe('check json-file.json', () => {
-        let mockModule;
-
-        before(() => {
-            mockModule = new MockModule('json-file', require('../../data/fixtures/mock_modules/json-file'));
-            // console.log(mockModule);
-        });
-
-        it('should return object', () => {
-            expect(mockModule).to.be.an('object');
-        });
-
-        it('should contain some fields', () => {
-            expect(mockModule).to.have.all.keys('name', 'module', 'config');
-        });
-
+    describe('check return-function-promise.js', () => {
         it('should return correct value', () => {
+            let mockModule = new MockModule('return-function-promise', require('../../data/fixtures/mock_modules/return-function-promise'));
+
             return mockModule.getResult()
                 .then((data) => {
                     expect(data).to.eql({
-                        name: 'json-file.json',
+                        name: 'return-function-promise',
+                        age: 16
+                    });
+                });
+        });
+    });
+
+    describe('check return-function-pure.js', () => {
+        it('should return correct value', () => {
+            let mockModule = new MockModule('return-function-pure', require('../../data/fixtures/mock_modules/return-function-pure'));
+
+            return mockModule.getResult()
+                .then((data) => {
+                    expect(data).to.eql({
+                        name: 'return-function-pure',
+                        age: 16
+                    });
+                });
+        });
+    });
+
+    describe('check return-function-with-param.js', () => {
+        it('should return correct value', () => {
+            let mockModule = new MockModule('return-function-with-param', require('../../data/fixtures/mock_modules/return-function-with-param'));
+
+            return mockModule.getResult('name-a', 2, 'not-exist')
+                .then((data) => {
+                    expect(data).to.eql({
+                        name: 'return-function-with-param',
+                        desc: 'a=name-a,b=2',
+                        age: 16
+                    });
+                });
+        });
+    });
+
+    describe('check return-plain-object.js', () => {
+        it('should return correct value', () => {
+            let mockModule = new MockModule('return-plain-object', require('../../data/fixtures/mock_modules/return-plain-object'));
+
+            return mockModule.getResult()
+                .then((data) => {
+                    expect(data).to.eql({
+                        name: 'return-plain-object',
+                        age: 16
+                    });
+                });
+        });
+    });
+
+    describe('check return-promise.js', () => {
+        it('should return correct value', () => {
+            let mockModule = new MockModule('return-promise', require('../../data/fixtures/mock_modules/return-promise'));
+
+            return mockModule.getResult()
+                .then((data) => {
+                    expect(data).to.eql({
+                        name: 'return-promise',
                         age: 16
                     });
                 });
