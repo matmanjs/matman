0b969d48881765f6821c49c45fc201340aa51ef3
helinjiang <me@helinjiang.com>
2018-06-03 21:31:23 +0800

    refactor: MockModuleConfig 的测试用例

--- test/scripts/mocker/MockModuleConfig.test.js
+++ test/scripts/mocker/MockModuleConfig.test.js
@@ -33,5 +33,33 @@ describe('./mocker/MockModuleConfig', () => {
         });
     });
 
+    describe('check return-function-promise', () => {
+        let mockModuleConfig;
+
+        before(() => {
+            mockModuleConfig = new MockModuleConfig('return-function-promise', require('../../data/fixtures/mock_modules/return-function-promise'));
+            // console.log(mockModuleConfig);
+        });
+
+        it('should return object', () => {
+            expect(mockModuleConfig).to.be.an('object');
+        });
+
+        it('should contain some fields', () => {
+            expect(mockModuleConfig).to.have.all.keys('name', 'description', 'priority', 'delay', 'query');
+        });
+
+        it('should equal value', () => {
+            expect(mockModuleConfig).to.eql({
+                name: 'return-function-promise',
+                description: 'return-function-promise',
+                priority: 0,
+                delay: 0,
+                query: {
+                    _m_target: 'return-function-promise'
+                }
+            });
+        });
+    });
 });
 
