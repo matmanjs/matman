c32de63ca4b9ace142c42818f835dece72d9a632
helinjiang <me@helinjiang.com>
2018-06-03 22:11:16 +0800

    refactor: MockModuleConfig 的测试用例

--- test/scripts/mocker/MockModuleConfig.test.js
+++ test/scripts/mocker/MockModuleConfig.test.js
@@ -102,5 +102,34 @@ describe('./mocker/MockModuleConfig', () => {
             });
         });
     });
+
+    describe('check no-config', () => {
+        it('should equal correct value', () => {
+            expect(new MockModuleConfig('no-config')).to.eql({
+                name: 'no-config',
+                description: 'no-config',
+                priority: 0,
+                delay: 0,
+                query: {
+                    _m_target: 'no-config'
+                }
+            });
+        });
+    });
+
+    describe('check exist-config', () => {
+        it('should equal correct value', () => {
+            expect(new MockModuleConfig('exist-config', require('../../data/fixtures/mock_modules/exist-config/config'))).to.eql({
+                name: 'exist-config',
+                description: 'exist-config description',
+                priority: 88,
+                delay: 10086,
+                query: {
+                    _m_target: 'exist-config',
+                    type: 1
+                }
+            });
+        });
+    });
 });
 
