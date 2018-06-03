73323f6db1c4d8f9acd11867651cb44b38509c6c
helinjiang <me@helinjiang.com>
2018-06-04 00:33:16 +0800

    refactor: 修改demo

--- demo/91_tmp/t1.js
+++ demo/91_tmp/t1.js
@@ -9,15 +9,15 @@ const mockerParser = new MockerParser({
 // let mockerList = mockerParser.getAllMocker();
 
 // console.log(mockerList);
-console.log(mockerParser.getMockerByName('demo_simple'));
-console.log(mockerParser.getMockModuleByName('demo_simple', 'error'));
+// console.log(mockerParser.getMockerByName('demo_simple'));
+// console.log(mockerParser.getMockModuleByName('demo_simple', 'error'));
 
-// console.log(mockerParser.getMockerByName('name_demo_basic'));
-//
-// console.log('\n');
-//
-// mockerParser.getMockModuleByName('name_demo_basic', 'success_4').getResult().then((data) => {
-//     console.log(data);
-// }).catch((err) => {
-//     console.error(err);
-// });
+console.log(mockerParser.getMockerByName('name_demo_basic'));
+
+console.log('\n');
+
+mockerParser.getMockModuleByName('name_demo_basic', 'success_4').getResult().then((data) => {
+    console.log(data);
+}).catch((err) => {
+    console.error(err);
+});
