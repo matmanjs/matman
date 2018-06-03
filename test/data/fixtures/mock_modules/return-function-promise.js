0786c71764b28156fab7e308862fd6d9e69c2e73
helinjiang <onehlj@qq.com>
2018-06-03 18:55:19 +0800

    refactor: 新增测试用例

---
+++ test/data/fixtures/mock_modules/return-function-promise.js
@@ -0,0 +1,6 @@
+module.exports = () => {
+    return Promise.resolve({
+        name: 'return-function-promise',
+        age: 16
+    });
+};
---
+++ test/data/fixtures/mock_modules/return-function-pure.js
@@ -0,0 +1,6 @@
+module.exports = () => {
+    return {
+        name: 'return-function-pure',
+        age: 16
+    };
+};
