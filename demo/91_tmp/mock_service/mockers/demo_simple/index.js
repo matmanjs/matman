c241821c262cedc1e47d6af94b94a9de2d5befb4
helinjiang <me@helinjiang.com>
2018-06-04 00:11:56 +0800

    refactor: 新增 Mocker

--- demo/91_tmp/mock_service/mockers/demo_simple/index.js
+++ demo/91_tmp/mock_service/mockers/demo_simple/index.js
@@ -1,40 +1,15 @@
-const MockModule = require('../../../../../src/mocker/MockModule');
-const MockerConfig = require('../../../../../src/mocker/MockerConfig');
+const Mocker = require('../../../../../src/mocker/Mocker');
 
-const config = require('./config');
+let modules = [{
+    name: 'error',
+    hasConfig: false
+}, {
+    name: 'success',
+    hasConfig: false
+}];
 
-/**
- * 是否序列化之后的标准 mocker 模块
- * @type {Boolean}
- */
-const isSerialized = true;
-
-/**
- * 该模块的根目录
- * @type {String}
- */
-const ROOT_PATH = __dirname;
-
-/**
- * 模块名字，用于区别不同的模块，建议不重复
- * @type {String}
- */
-const name = config.name || 'demo_simple';
-
-/**
- * mock module 配置列表
- * @type {Array}
- */
-const mockModuleList = [];
-
-mockModuleList.push(new MockModule('error', require('./mock_modules/error')));
-mockModuleList.push(new MockModule('success', require('./mock_modules/success')));
-
-module.exports = {
-    name: name,
-    isSerialized: isSerialized,
-    ROOT_PATH: ROOT_PATH,
-    config: new MockerConfig(name, config, mockModuleList),
-    mockModuleList: mockModuleList
-};
+module.exports = new Mocker(__dirname, {
+    defaultName: 'demo_simple',
+    modules: modules
+});
 
