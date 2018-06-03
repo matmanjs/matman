6588e2da901baa0fad7ebf12c53c20a58ba3a3ab
helinjiang <me@helinjiang.com>
2018-06-03 21:40:17 +0800

    refactor: MockModule 的测试用例

---
+++ test/scripts/mocker/MockModule.test.js
@@ -0,0 +1,35 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const MockModule = require('../../../src/mocker/MockModule');
+
+describe('./mocker/MockModule', () => {
+    describe('check json-file.json', () => {
+        let mockModule;
+
+        before(() => {
+            mockModule = new MockModule('json-file', require('../../data/fixtures/mock_modules/json-file'));
+            // console.log(mockModule);
+        });
+
+        it('should return object', () => {
+            expect(mockModule).to.be.an('object');
+        });
+
+        it('should contain some fields', () => {
+            expect(mockModule).to.have.all.keys('name', 'module', 'config');
+        });
+
+        it('should return correct value', () => {
+            return mockModule.getResult()
+                .then((data) => {
+                    expect(data).to.eql({
+                        name: 'json-file.json',
+                        age: 16
+                    });
+                });
+        });
+    });
+
+});
+
