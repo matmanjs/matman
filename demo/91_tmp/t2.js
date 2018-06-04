2c70d1c5caecc40ff031a2e11cad9ed8c5d0adbe
linjianghe <linjianghe@tencent.com>
2018-06-04 17:12:47 +0800

    refactor: 简化 mocker 的操作

--- demo/91_tmp/t2.js
+++ demo/91_tmp/t2.js
@@ -1,28 +1,6 @@
-function testPromise() {
-    return Promise.reject('testPromisexxx');
-}
+const path = require('path');
 
-function testNormal() {
-    return 'testNormal';
-}
-
-const testData = 'testData';
-const testPromiseData = Promise.resolve('testPromiseData');
-
-Promise.resolve(testPromise()).then((data) => {
-    console.log('\n', data);
-}).catch((e)=>{
-    console.log('\n', e);
-})
-
-Promise.resolve(testNormal()).then((data) => {
-    console.log('\n', data);
-});
-
-Promise.resolve(testData).then((data) => {
-    console.log('\n', data);
-});
-
-Promise.resolve(testPromiseData).then((data) => {
-    console.log('\n', data);
-});
+console.log(__dirname)
+console.log(path.basename(__dirname))
+console.log(path.basename('a/b/c.json'))
+console.log(path.basename('a/vc/a.js','.json'))
