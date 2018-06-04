fa972b6adeb90991c0d516dfee9a7d280e36135b
linjianghe <linjianghe@tencent.com>
2018-06-04 17:46:41 +0800

    refactor: 新增测试 Mocker.test.js

---
+++ test/data/fixtures/mock_service/mockers/demo_01/index.js
@@ -0,0 +1,4 @@
+const Mocker = require('../../../../../../src/mocker/Mocker');
+
+module.exports = new Mocker(__dirname);
+
---
+++ test/scripts/mocker/Mocker.test.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const chai = require('chai');
+const expect = chai.expect;
+
+const Mocker = require('../../../src/mocker/Mocker');
+const MockModule = require('../../../src/mocker/MockModule');
+const MockerConfig = require('../../../src/mocker/MockerConfig');
+
+describe.only('./mocker/Mocker.js', () => {
+    describe('check demo_01', () => {
+        let mocker;
+
+        before(() => {
+            mocker = new Mocker(path.resolve(__dirname, '../../data/fixtures/mock_service/mockers/demo_01'));
+            // console.log(mocker);
+        });
+
+        it('should be instanceof Mocker ', () => {
+            expect(mocker).to.be.an.instanceof(Mocker);
+        });
+
+        it('should contain some fields', () => {
+            expect(mocker).to.have.all.keys('basePath', 'name', 'mockModuleList', 'config');
+        });
+
+        it('mocker.basePath should not empty ', () => {
+            expect(mocker.basePath).to.be.a('string').and.not.empty;
+        });
+
+        it('mocker.name should be correct ', () => {
+            expect(mocker.name).to.equal('demo_01');
+        });
+
+        it('mocker.config should be instanceof MockerConfig ', () => {
+            expect(mocker.config).to.be.an.instanceof(MockerConfig);
+        });
+
+        it('mocker.mockModuleList item should be instanceof MockModule ', () => {
+            expect(mocker.mockModuleList[0]).to.be.an.instanceof(MockModule);
+        });
+
+        it('mocker.mockModuleList has 2 items', () => {
+            expect(mocker.mockModuleList).to.have.lengthOf(2);
+        });
+    });
+});
+
