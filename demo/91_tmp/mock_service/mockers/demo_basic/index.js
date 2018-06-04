fb4245ac236e3fcddd7c220ab1fff79253c43b81
helinjiang <onehlj@qq.com>
2018-06-04 17:25:47 +0800

    refactor: 简化 mocker 的操作

--- demo/91_tmp/mock_service/mockers/demo_basic/index.js
+++ demo/91_tmp/mock_service/mockers/demo_basic/index.js
@@ -1,29 +1,4 @@
 const Mocker = require('../../../../../src/mocker/Mocker');
 
-// 默认名字
-let defaultName = 'demo_basic';
-
-// mock module 的配置信息
-let modules = [{
-    name: 'success_2',
-    hasConfig: false
-}, {
-    name: 'success_3_renamed',
-    hasConfig: true
-}, {
-    name: 'success_4',
-    hasConfig: true
-}, {
-    name: 'error',
-    hasConfig: false
-}, {
-    name: 'success_1',
-    hasConfig: false
-}];
-
-module.exports = new Mocker(__dirname, {
-    defaultName: defaultName,
-    modules: modules
-});
-
+module.exports = new Mocker(__dirname);
 
