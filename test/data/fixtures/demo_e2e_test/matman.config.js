49f8afad228c77a549e8b6df9a98a2211074588e
helinjiang <onehlj@qq.com>
2018-06-20 01:01:16 +0800

    refactor: 修改 demo

--- test/data/fixtures/demo_e2e_test/matman.config.js
+++ test/data/fixtures/demo_e2e_test/matman.config.js
@@ -1,9 +1,1 @@
-const path = require('path');
-
-module.exports = {
-  clientScriptBuildPath: path.join(__dirname, 'dist-client-script'),
-  clientScriptMatch: /crawlers\/.*\.js$/,
-  entry: {
-    'page_rule/crawlers/get-page-info': path.join(__dirname, 'e2e_test/targets/page_rule/crawlers/get-page-info.js')
-  }
-};
+module.exports = {};
