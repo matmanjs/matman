a29dfa542885b46a738644758b9b4dd96d6e99d1
linjianghe <linjianghe@tencent.com>
2018-06-28 12:42:29 +0800

    feat: 增加 screenshot 截图功能 #99

--- demo/91_tmp/t2.js
+++ demo/91_tmp/t2.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 console.log(__dirname)
+console.log(__filename)
 console.log(path.basename(__dirname))
 console.log(path.basename('a/b/c.json'))
 console.log(path.basename('a/vc/a.js','.json'))
