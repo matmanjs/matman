a67a9f239f66900d214288853dff92eb0d73466d
linjianghe <linjianghe@tencent.com>
2018-07-02 21:37:08 +0800

    refactor: 删除冗余代码

--- test/data/fixtures/parser/demo_handle_modules/handle_modules/success_1.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  "result": 1
-};
--- test/data/fixtures/parser/demo_handle_modules/handle_modules/success_4/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (params, req) {
-  if (params && params.a) {
-    return 'from_param_' + params.a;
-  }
-
-  return 4;
-};
