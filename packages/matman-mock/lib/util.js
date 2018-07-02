054a53036f9ea63ca1527a5118aa9a14690b667f
linjianghe <linjianghe@tencent.com>
2018-07-02 17:48:38 +0800

    refactor: 重新 matman-mock 的函数导出

--- packages/matman-mock/lib/util.js
+++ packages/matman-mock/lib/util.js
@@ -9,7 +9,7 @@ var _require = require('../config'),
 var MatmanQueryItem = require('./model/MatmanQueryItem');
 
 /**
- * 获得 req.headers.referer 中携带的额外参数
+ * 获得 req.headers.referer 中携带的额外参数列表
  *
  * @param {String} referer req.headers.referer
  * @returns {{_m_name:String,_m_target:String,_m_disable:Number}[]} 结果
@@ -52,29 +52,34 @@ function getMatmanQueryItem(referer, name) {
 
 /**
  * 获得 mock server 的根目录绝对路径
+ *
  * @param {String} rootPath 项目根目录
  * @param {String} mockServerPath
  * @returns {String}
  */
 function getMockServerBasePath(rootPath, mockServerPath) {
   var result = mockServerPath || './mock_server';
+
   if (!path.isAbsolute(result)) {
     result = path.resolve(rootPath, result);
   }
+
   return result;
 }
 
 /**
- * 获得 mock server 的根目录绝对路径
+ * 获得 mock server 的构建之后的根目录绝对路径
  * @param {String} rootPath 项目根目录
  * @param {String} buildPath
  * @returns {String}
  */
 function getMockServerBuildPath(rootPath, buildPath) {
   var result = buildPath || './build';
+
   if (!path.isAbsolute(result)) {
     result = path.resolve(rootPath, result);
   }
+
   return result;
 }
 
