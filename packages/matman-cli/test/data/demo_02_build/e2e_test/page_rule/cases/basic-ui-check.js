edae4e3ee89b00d81f082ae33a44f1623e108fd1
helinjiang <onehlj@qq.com>
2018-07-02 21:29:25 +0800

    fix: 修改 demo_02_build

--- packages/matman-cli/test/data/demo_02_build/e2e_test/page_rule/cases/basic-ui-check.js
+++ packages/matman-cli/test/data/demo_02_build/e2e_test/page_rule/cases/basic-ui-check.js
@@ -3,16 +3,16 @@ const matman = require('matman');
 const env = require('./env/index');
 
 function getResult(opts) {
-    // 如何校验，前端页面执行脚本
-    let preloadClientScriptPath = matman.getPreloadScriptPath('page_rule/crawlers/get-page-info');
+  // 如何校验，前端页面执行脚本
+  let preloadClientScriptPath = matman.getPreloadScriptPath('page_rule/crawlers/get-page-info', __dirname);
 
-    opts = Object.assign({
-        // proxyServer: env.OPTS.PROXY_SERVER_DEV,
-        wait: env.OPTS.WAIT,
-        screenshot: __filename
-    }, opts);
+  opts = Object.assign({
+    // proxyServer: env.OPTS.PROXY_SERVER_DEV,
+    wait: env.OPTS.WAIT,
+    screenshot: __filename
+  }, opts);
 
-    return matman.e2e.scan(env.getPageUrl(true), preloadClientScriptPath, opts);
+  return matman.e2e.scan(env.getPageUrl(true), preloadClientScriptPath, opts);
 }
 
 module.exports = getResult;
--- packages/matman-cli/test/data/demo_02_build/matman.config.js
+++ packages/matman-cli/test/data/demo_02_build/matman.config.js
@@ -3,8 +3,6 @@ const path = require('path');
 module.exports = {
   rootPath: __dirname,
   buildPath: path.resolve(__dirname, './build'),
-  logPath: path.resolve(__dirname, './build/logs'),
-  mockServerPath: path.resolve(__dirname, '../../../../matman-mock/test/data/fixtures/mock_server/mockers'),
   e2eTestPath: path.resolve(__dirname, './e2e_test'),
   clientScriptBuildPath: path.join(__dirname, './build/client-script'),
   clientScriptMatch: /crawlers\/.*\.js$/
