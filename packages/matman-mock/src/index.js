f77ad4c19791a6d0865dec869b631f038cf63fdb
helinjiang <onehlj@qq.com>
2018-07-02 17:26:37 +0800

    feat: 增加 babel compile

--- packages/matman-mock/src/index.js
+++ packages/matman-mock/src/index.js
@@ -5,14 +5,5 @@ module.exports = {
   Mocker: require('./model/Mocker'),
   MockerParser: require('./model/MockerParser'),
   MatmanQuery: matmanQuery.MatmanQuery,
-  QUERY_KEY: matmanQuery.QUERY_KEY,
-  mockerUtil: require('./util'),
-  ClientScript: require('./tester/ClientScript'),
-  E2eTestAction: require('./tester/E2eTestAction'),
-  getPreloadScriptPath: require('./tester/get-preload-script-path'),
-  getBuildPath: require('./tester/get-build-path'),
-  getConfigFilePath: require('./tester/get-config-file-path'),
-  e2e: {
-    scan: require('./tester/e2e-test-scan-page')
-  }
+  QUERY_KEY: matmanQuery.QUERY_KEY
 };
