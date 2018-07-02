084c43c690a1bf1900c120dfaadd210993e7e383
helinjiang <onehlj@qq.com>
2018-07-03 00:13:23 +0800

    feat: 新增 action.operate 操作

--- packages/matman-e2e/lib/action/scan.js
+++ packages/matman-e2e/lib/action/scan.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var BasicActionWithClientScript = require('../model/BasicActionWithClientScript');
+var operate = require('./operate');
 
 /**
  * 适合简单的页面扫描场景，无交互行为。
@@ -13,13 +13,13 @@ var BasicActionWithClientScript = require('../model/BasicActionWithClientScript'
 function scan(pageUrl, preloadClientScriptPath) {
   var opts = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
 
-  var actionHandle = new BasicActionWithClientScript(pageUrl, preloadClientScriptPath, opts);
+  return operate(pageUrl, preloadClientScriptPath, opts, function (testAction) {
 
-  actionHandle.addAction(function (nightmareRun) {
-    return nightmareRun.wait(opts.wait || 500);
-  });
-
-  return actionHandle.getResult().then(function (result) {
+    // scan 行为是一种特殊的操作，因为它只有一个行为，且结果也不再是数组
+    testAction.addAction(function (nightmareRun) {
+      return nightmareRun.wait(opts.wait || 500);
+    });
+  }).then(function (result) {
     // 去掉这个nightmare的返回。目前他没有什么其他的用处，但是在 JSON.stringify 时会报错
     if (result.globalInfo && result.globalInfo.recorder && result.globalInfo.recorder.nightmare) {
       delete result.globalInfo.recorder.nightmare;
