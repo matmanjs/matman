8f0113b3f82f7caa9d2181f0cb100e269c342146
linjianghe <linjianghe@tencent.com>
2018-10-09 21:50:22 +0800

    feat: 使之异步，避免 websocket 还未连接成功

--- packages/matman-stub-async/lib/util.js
+++ packages/matman-stub-async/lib/util.js
@@ -76,23 +76,27 @@ function recieve(asyncClient, route, params) {
  */
 function _check(asyncClient, route) {
     return new Promise(function (resolve, reject) {
-        // 有问题时直接执行真实的fetch方法
-        if (!asyncClient) {
-            return reject(RESULT.NO_ASYNC_CLIENT);
-        }
 
-        // 如果远程服务未启动也需要放弃stub
-        if (!asyncClient.isConnected()) {
-            alert('matman stub \u670D\u52A1\u672A\u542F\u52A8\uFF01\u8BF7\u68C0\u67E5 ' + asyncClient.getURI() + ' \u662F\u5426\u5DF2\u542F\u52A8');
-            return reject(RESULT.NOT_CONNECTED);
-        }
+        // 使之异步，避免 websocket 还未连接成功
+        setTimeout(function () {
+            // 有问题时直接执行真实的fetch方法
+            if (!asyncClient) {
+                return reject(RESULT.NO_ASYNC_CLIENT);
+            }
 
-        // 如果远程服务未启动也需要放弃stub
-        if (!route) {
-            return reject(RESULT.NO_ROUTE);
-        }
+            // 如果远程服务未启动也需要放弃stub
+            if (!asyncClient.isConnected()) {
+                alert('matman stub \u670D\u52A1\u672A\u542F\u52A8\uFF01\u8BF7\u68C0\u67E5 ' + asyncClient.getURI() + ' \u662F\u5426\u5DF2\u542F\u52A8');
+                return reject(RESULT.NOT_CONNECTED);
+            }
 
-        resolve();
+            // 如果远程服务未启动也需要放弃stub
+            if (!route) {
+                return reject(RESULT.NO_ROUTE);
+            }
+
+            resolve();
+        }, 10);
     });
 }
 
