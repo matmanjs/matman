ad69d9a5cd5614dd15ec9a5bcd53fa2036593557
linjianghe <linjianghe@tencent.com>
2018-10-10 17:52:52 +0800

    refactor: 优化检查是否链接成功的判断逻辑

--- packages/matman-stub-async/lib/util.js
+++ packages/matman-stub-async/lib/util.js
@@ -66,27 +66,45 @@ function receive(asyncClient, route, callback) {
  */
 function _check(asyncClient, route) {
   return new Promise(function (resolve, reject) {
+    // 有问题时直接执行真实的fetch方法
+    if (!asyncClient) {
+      return reject(RESULT.NO_ASYNC_CLIENT);
+    }
 
-    // 使之异步，避免 websocket 还未连接成功
-    setTimeout(function () {
-      // 有问题时直接执行真实的fetch方法
-      if (!asyncClient) {
-        return reject(RESULT.NO_ASYNC_CLIENT);
-      }
+    // 如果远程服务未启动也需要放弃stub
+    if (!route) {
+      return reject(RESULT.NO_ROUTE);
+    }
 
-      // 如果远程服务未启动也需要放弃stub
-      if (!asyncClient.isConnected()) {
+    var i = 0;
+    var checkT = void 0;
+
+    function checkConnect() {
+      if (asyncClient.isConnected()) {
+        // 如果已经连接成功，则清除定时器，并返回
+        if (checkT) {
+          clearTimeout(checkT);
+        }
+
+        resolve();
+      } else if (i > 9) {
+        // 如果重试次数大于了10次，也终止，清除定时器，并返回
+        if (checkT) {
+          clearTimeout(checkT);
+        }
+
         alert('matman stub \u670D\u52A1\u672A\u542F\u52A8\uFF01\u8BF7\u68C0\u67E5 ' + asyncClient.getURI() + ' \u662F\u5426\u5DF2\u542F\u52A8');
-        return reject(RESULT.NOT_CONNECTED);
+        reject(RESULT.NOT_CONNECTED);
+      } else {
+        i++;
+        // 使之异步，避免 websocket 还未连接成功
+        checkT = setTimeout(function () {
+          checkConnect();
+        }, 10);
       }
+    }
 
-      // 如果远程服务未启动也需要放弃stub
-      if (!route) {
-        return reject(RESULT.NO_ROUTE);
-      }
-
-      resolve();
-    }, 10);
+    checkConnect();
   });
 }
 
