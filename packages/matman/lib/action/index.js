3024f267c63696fdbf8bc9b47fa91b9926fec7c0
helinjiang <me@helinjiang.com>
2018-11-05 00:19:35 +0800

    refactor: lib 不提交到 git 仓库

--- packages/matman/lib/action/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  scan: require('./scan'),
-  operate: require('./operate')
-};
-//# sourceMappingURL=index.js.map
--- packages/matman/lib/util/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  getPreloadScriptPath: require('./get-preload-script-path'),
-  getBuildPath: require('./get-build-path'),
-  getConfigFilePath: require('./get-config-file-path')
-};
-//# sourceMappingURL=index.js.map
