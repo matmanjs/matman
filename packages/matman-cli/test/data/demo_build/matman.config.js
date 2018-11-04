49aadc06725c6429701ab889c41f634391ac69bb
linjianghe <linjianghe@tencent.com>
2018-11-04 18:23:40 +0800

    refactor: 优化测试用例

--- packages/matman-cli/test/data/demo_build/matman.config.js
+++ packages/matman-cli/test/data/demo_build/matman.config.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 module.exports = {
   rootPath: __dirname,
-  buildPath: path.resolve(__dirname, './build'),
   e2eTestPath: path.resolve(__dirname, './e2e_test'),
   clientScriptBuildPath: path.join(__dirname, './build/client-script'),
-  clientScriptMatch: /crawlers\/.*\.js$/
+  clientScriptMatch: /crawlers[\/|\\].*\.js$/
 };
