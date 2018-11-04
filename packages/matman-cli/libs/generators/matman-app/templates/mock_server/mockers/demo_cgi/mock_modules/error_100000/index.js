bf76d356db00d0161d6c0b3c1e84bd6ce08fea34
helinjiang <onehlj@qq.com>
2018-11-04 19:01:53 +0800

    refactor: 删除冗余代码

--- packages/matman-cli/libs/generators/matman-app/templates/mock_server/mockers/demo_cgi/mock_modules/error_100000/index.js
+++
@@ -1,5 +0,0 @@
-const { getFail } = require('../../base');
-
-module.exports = function () {
-    return getFail(100000, 'error msg!');
-};
--- packages/matman-cli/libs/generators/npm-package/templates/_README.md
+++
@@ -1,5 +0,0 @@
-# <%= name %>
-
-> 本项目需要配合 [matman](https://github.com/helinjiang/matman) 使用。
-
-<%= description %>
