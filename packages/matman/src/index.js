fa9e7924cea60031aee3791bfafa829d64b854e3
helinjiang <onehlj@qq.com>
2018-11-06 09:40:56 +0800

    refactor: 将 CrawlerParser 移至 matman-crawler 组件内

--- packages/matman/src/index.js
+++ packages/matman/src/index.js
@@ -1,11 +1,9 @@
-import CrawlerParser from './model/CrawlerParser';
 import CaseParser from './model/CaseParser';
 import BaseHandle from './model/BaseHandle';
 
 import { getConfigFilePath } from './util';
 
 module.exports = {
-    CrawlerParser: CrawlerParser,
     CaseParser: CaseParser,
     BaseHandle: BaseHandle,
     getConfigFilePath: getConfigFilePath
