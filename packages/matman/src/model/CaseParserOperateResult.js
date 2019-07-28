c0a5c6d7cea26da223b622e6dca83c6bcafb9472
helinjiang <onehlj@qq.com>
2019-07-28 16:44:04 +0800

    feat: 获取到结果队列和网络请求队列

--- packages/matman/src/model/CaseParserOperateResult.js
+++ packages/matman/src/model/CaseParserOperateResult.js
@@ -1,8 +1,8 @@
 export default class CaseParserOperateResult {
     constructor(baseHandleResult) {
-        this.data = baseHandleResult.data;
-        this._dataIndexMap = baseHandleResult._dataIndexMap;
-        this.globalInfo = baseHandleResult.globalInfo;
+        this.data = baseHandleResult.data || [];
+        this._dataIndexMap = baseHandleResult._dataIndexMap || {};
+        this.globalInfo = baseHandleResult.globalInfo || {};
     }
 
     /**
@@ -13,5 +13,52 @@ export default class CaseParserOperateResult {
     get(key) {
         const i = this._dataIndexMap[key];
         return this.data[typeof i === 'number' ? i : key];
+    }
+
+    /**
+     * 获得捕获到的请求队列
+     *
+     * @return {Array}
+     */
+    getQueue() {
+        return (this.globalInfo.recorder && this.globalInfo.recorder.queue) || [];
+    }
+
+    /**
+     * 获得网络请求
+     *
+     * @param {String} [resourceType] 资源类型，详见 nightmare-handler 组件中的 RESOURCE_TYPE
+     * {
+            MAIN_FRAME: 'mainFrame',
+            SUB_FRAME: 'subFrame',
+            STYLESHEET: 'stylesheet',
+            SCRIPT: 'script',
+            IMAGE: 'image',
+            OBJECT: 'object',
+            XHR: 'xhr',
+            OTHER: 'other'
+        }
+     * @return {Array}
+     */
+    getNetwork(resourceType) {
+        const queue = this.getQueue();
+
+        return queue.filter((item) => {
+            if (item.eventName !== 'did-get-response-details') {
+                return false;
+            }
+
+            if (!item.args || item.args.length < 4) {
+                return false;
+            }
+
+            // 如果定义了 resourceType 但又不匹配，则放弃之
+            if (resourceType && (item.args[item.args.length - 1] !== resourceType)) {
+                return false;
+            }
+
+            // 这是真实的请求 originalURL
+            return !!item.args[3];
+        });
     }
 };
