70cce2323f4841a94c0d6a624fdad343747b91fd
helinjiang <onehlj@qq.com>
2020-04-27 22:41:27 +0800

    feat: 增加 ScreenshotConfig.js 的测试用例

---
+++ packages/matman/test/scripts/model/ScreenshotConfig.test.js
@@ -0,0 +1,26 @@
+import path from 'path';
+import { expect } from 'chai';
+
+import ScreenshotConfig from '../../../src/model/ScreenshotConfig';
+
+describe('check model/ScreenshotConfig.js', () => {
+    describe('check constructor(opts, basePath, tag)', () => {
+        const MATMAN_ROOT_PATH = path.join(__dirname, '../../data/fixtures/baidu_01');
+        const BASE_PATH = path.join(MATMAN_ROOT_PATH, './src/page_baidu_index/cases/basic-check');
+
+        it('if opts is Boolean and tag is undefined', () => {
+            expect(new ScreenshotConfig(true, BASE_PATH)).to.eql({
+                path: path.join(MATMAN_ROOT_PATH, './build/screenshot/page_baidu_index_cases/basic-check.png'),
+                tag: undefined
+            });
+        });
+
+        it('if opts is Boolean and tag is "mytag"', () => {
+            expect(new ScreenshotConfig(true, BASE_PATH, 'mytag')).to.eql({
+                path: path.join(MATMAN_ROOT_PATH, './build/screenshot/page_baidu_index_cases/basic-check_mytag.png'),
+                tag: 'mytag'
+            });
+        });
+    });
+});
+
