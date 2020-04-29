6f4b8f8cba2ba7170e56233736fc939bdfece963
linjianghe <linjianghe@tencent.com>
2020-04-29 18:44:53 +0800

    feat: 补充 windows 的路径处理用例

--- packages/matman/test/scripts/util/index.test.js
+++ packages/matman/test/scripts/util/index.test.js
@@ -107,70 +107,110 @@ describe('./util/index.js', () => {
     });
 
     describe('check getFolderNameFromPath(targetPath)', () => {
-        it('targetPath is absolute folder', () => {
-            expect(getFolderNameFromPath('/home/i/am/absolute')).to.equal('_home_i_am_absolute');
+        it('linux: targetPath is absolute folder', () => {
+            expect(getFolderNameFromPath('/home/i/am/absolute')).to.equal('root_home_i_am_absolute');
         });
 
-        it('targetPath is absolute file', () => {
-            expect(getFolderNameFromPath('/home/i/am/absolute/myfile.png')).to.equal('_home_i_am_absolute_myfile_png');
+        it('linux: targetPath is absolute file', () => {
+            expect(getFolderNameFromPath('/home/i/am/absolute/myfile.png')).to.equal('root_home_i_am_absolute_myfile_png');
         });
 
-        it('targetPath is relative folder with ./', () => {
-            expect(getFolderNameFromPath('./i/am/relative/path')).to.equal('i_am_relative_path');
-        });
-
-        it('targetPath is relative folder with ../', () => {
-            expect(getFolderNameFromPath('../i/am/relative/path')).to.equal('parent_i_am_relative_path');
-        });
-
-        it('targetPath is relative folder with ../../', () => {
-            expect(getFolderNameFromPath('../../i/am/relative/path')).to.equal('parent_parent_i_am_relative_path');
-        });
-
-        it('targetPath is relative folder', () => {
+        it('linux: targetPath is relative folder', () => {
             expect(getFolderNameFromPath('i/am/relative/path')).to.equal('i_am_relative_path');
         });
 
-        it('targetPath is fileName with extname', () => {
+        it('linux: targetPath is relative folder with ./', () => {
+            expect(getFolderNameFromPath('./i/am/relative/path')).to.equal('i_am_relative_path');
+        });
+
+        it('linux: targetPath is relative folder with ../', () => {
+            expect(getFolderNameFromPath('../i/am/relative/path')).to.equal('parent_i_am_relative_path');
+        });
+
+        it('linux: targetPath is relative folder with ../../', () => {
+            expect(getFolderNameFromPath('../../i/am/relative/path')).to.equal('parent_parent_i_am_relative_path');
+        });
+
+        it('linux: targetPath is fileName with extname', () => {
             expect(getFolderNameFromPath('myscript.js')).to.equal('myscript_js');
         });
 
-        it('targetPath is fileName without extname', () => {
+        it('linux: targetPath is fileName without extname', () => {
             expect(getFolderNameFromPath('myscript')).to.equal('myscript');
+        });
+
+        it('windows: targetPath is absolute folder', () => {
+            expect(getFolderNameFromPath('d:\\i\\am\\absolute')).to.equal('d_i_am_absolute');
+        });
+
+        it('windows: targetPath is relative folder', () => {
+            expect(getFolderNameFromPath('i\\am\\relative\\path')).to.equal('i_am_relative_path');
+        });
+
+        it('windows: targetPath is relative folder with ./', () => {
+            expect(getFolderNameFromPath('.\\i\\am\\relative\\path')).to.equal('i_am_relative_path');
+        });
+
+        it('windows: targetPath is relative folder with ../', () => {
+            expect(getFolderNameFromPath('..\\i\\am\\relative\\path')).to.equal('parent_i_am_relative_path');
+        });
+
+        it('windows: targetPath is relative folder with ../../', () => {
+            expect(getFolderNameFromPath('..\\..\\i\\am\\relative\\path')).to.equal('parent_parent_i_am_relative_path');
         });
     });
 
-    describe('check getSaveDirFromPath(relativePath)', () => {
-        it('relativePath is absolute folder', () => {
+    describe('check getSaveDirFromPath(targetPath)', () => {
+        it('linux: targetPath is absolute folder', () => {
             expect(getSaveDirFromPath('/home/i/am/absolute')).to.equal('root/home/i/am/absolute');
         });
 
-        it('relativePath is absolute file', () => {
+        it('linux: targetPath is absolute file', () => {
             expect(getSaveDirFromPath('/home/i/am/absolute/myfile.png')).to.equal('root/home/i/am/absolute/myfile.png');
         });
 
-        it('relativePath is relative folder with ./', () => {
-            expect(getSaveDirFromPath('./i/am/relative/path')).to.equal('i/am/relative/path');
-        });
-
-        it('relativePath is relative folder with ../', () => {
-            expect(getSaveDirFromPath('../i/am/relative/path')).to.equal('parent/i/am/relative/path');
-        });
-
-        it('relativePath is relative folder with ../../', () => {
-            expect(getSaveDirFromPath('../../i/am/relative/path')).to.equal('parent/parent/i/am/relative/path');
-        });
-
-        it('relativePath is relative folder', () => {
+        it('linux: targetPath is relative folder', () => {
             expect(getSaveDirFromPath('i/am/relative/path')).to.equal('i/am/relative/path');
         });
 
-        it('targetPath is fileName with extname', () => {
+        it('linux: targetPath is relative folder with ./', () => {
+            expect(getSaveDirFromPath('./i/am/relative/path')).to.equal('i/am/relative/path');
+        });
+
+        it('linux: targetPath is relative folder with ../', () => {
+            expect(getSaveDirFromPath('../i/am/relative/path')).to.equal('parent/i/am/relative/path');
+        });
+
+        it('linux: targetPath is relative folder with ../../', () => {
+            expect(getSaveDirFromPath('../../i/am/relative/path')).to.equal('parent/parent/i/am/relative/path');
+        });
+
+        it('linux: targetPath is fileName with extname', () => {
             expect(getSaveDirFromPath('myscript.js')).to.equal('myscript.js');
         });
 
-        it('targetPath is fileName without extname', () => {
+        it('linux: targetPath is fileName without extname', () => {
             expect(getSaveDirFromPath('myscript')).to.equal('myscript');
+        });
+
+        it('windows: targetPath is absolute folder', () => {
+            expect(getSaveDirFromPath('d:\\i\\am\\absolute')).to.equal('d\\i\\am\\absolute');
+        });
+
+        it('windows: targetPath is relative folder', () => {
+            expect(getSaveDirFromPath('i\\am\\relative\\path')).to.equal('i\\am\\relative\\path');
+        });
+
+        it('windows: targetPath is relative folder with ./', () => {
+            expect(getSaveDirFromPath('.\\i\\am\\relative\\path')).to.equal('i\\am\\relative\\path');
+        });
+
+        it('windows: targetPath is relative folder with ../', () => {
+            expect(getSaveDirFromPath('..\\i\\am\\relative\\path')).to.equal('parent\\i\\am\\relative\\path');
+        });
+
+        it('windows: targetPath is relative folder with ../../', () => {
+            expect(getSaveDirFromPath('..\\..\\i\\am\\relative\\path')).to.equal('parent\\parent\\i\\am\\relative\\path');
         });
     });
 });
