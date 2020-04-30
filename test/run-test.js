9ae531a3c75f364f2fc102d17c775370642b5577
helinjiang <onehlj@qq.com>
2020-04-30 20:29:55 +0800

    refactor: 优化 run-test.js

--- test/run-test.js
+++ test/run-test.js
@@ -1,43 +1,103 @@
-// 测试之前需要将各个组件进行构建
-// 测试之前需要进行构建，因为没有用 babel 处理
-// 测试结束之后需要移除所有的 build 文件夹？
-
 const fs = require('fs');
 const path = require('path');
 const runCmd = require('./run-cmd');
 
-const HI_DEMO_ROOT = path.join(__dirname, '../packages/matman/test/data/hi-demo');
-const demoArr = fs.readdirSync(HI_DEMO_ROOT);
 const promiseList = [];
 
-demoArr.forEach((demoName) => {
+/**
+ * 运行 lerna bootstrap 来初始
+ * @return {Promise}
+ */
+function runLernaBootstrap() {
+    const t = Date.now();
 
-    //获取当前文件的绝对路径
-    const fileDir = path.join(HI_DEMO_ROOT, demoName);
+    console.error('开始执行 lerna bootstrap ...');
 
-    if (!fs.statSync(fileDir).isDirectory()) {
-        return;
-    }
+    return runCmd.runByExec('lerna bootstrap', { cwd: path.join(__dirname, '../') })
+        .then((data) => {
+            console.log(`lerna bootstrap 执行执行完成！耗时${Date.now() - t}ms`);
+            return data;
+        })
+        .catch((err) => {
+            console.error('lerna bootstrap 失败', err);
 
-    promiseList.push(runCmd.runByExec('npm run build', { cwd: fileDir }));
-});
+            return Promise.reject(err);
+        });
+}
 
-console.log(`开始执行，数量为：${promiseList.length} 个！`);
+/**
+ * 执行构建
+ *
+ * @return {Promise<>}
+ */
+function runBuild() {
+    const t = Date.now();
 
-const startT = Date.now();
-Promise.all(promiseList)
+    console.error('开始执行构建...');
+
+    // 自动获取所有参与测试demo的路径
+    const HI_DEMO_ROOT = path.join(__dirname, '../packages/matman/test/data/hi-demo');
+    const demoArr = fs.readdirSync(HI_DEMO_ROOT);
+
+    demoArr.forEach((demoName) => {
+        //获取当前文件的绝对路径
+        const fileDir = path.join(HI_DEMO_ROOT, demoName);
+
+        if (!fs.statSync(fileDir).isDirectory()) {
+            return;
+        }
+
+        promiseList.push(runCmd.runByExec('npm run build', { cwd: fileDir }));
+    });
+
+    console.log(`待测试的 demo 总数量为：${promiseList.length} 个！`);
+
+    return Promise.all(promiseList)
+        .then((data) => {
+            console.log(`构建完成，耗时${Date.now() - t}ms`);
+            return data;
+        })
+        .catch((err) => {
+            console.error('构建项目时失败', err);
+            return Promise.reject(err);
+        });
+}
+
+/**
+ * 执行测试用例
+ *
+ * @return {Promise<>}
+ */
+function runTestDirect() {
+    const t = Date.now();
+
+    console.error('开始执行测试用例...');
+
+    return runCmd.runByExec('npm run test:direct', { cwd: path.join(__dirname, '../') })
+        .then((data) => {
+            console.log(`测试用例执行执行完成！耗时${Date.now() - t}ms`);
+            return data;
+        })
+        .catch((err) => {
+            console.error('执行测试时失败', err);
+            return Promise.reject(err);
+        });
+}
+
+console.log(`============开始执行===========`);
+
+const t = Date.now();
+
+runLernaBootstrap()
     .then((data) => {
-        const buildCompleteT = Date.now();
-        console.log(`构建完成，耗时${buildCompleteT - startT}ms，开始执行测试用例...`);
-
-        runCmd.runByExec('npm run  test:direct', { cwd: path.join(__dirname, '../') })
+        return runBuild()
             .then((data) => {
-                console.log(`测试用例执行执行完成！耗时${Date.now() - buildCompleteT}ms`);
-            })
-            .catch((err) => {
-                console.error('执行测试时失败', err);
+                return runTestDirect()
+                    .then((data) => {
+                        console.log(`============执行结束，总耗时${Date.now() - t}ms===========`);
+                    });
             });
     })
-    .catch((err) => {
-        console.error('构建项目时失败', err);
+    .catch(() => {
+        console.log(`============执行过程遇到异常，总耗时${Date.now() - t}ms===========`);
     });
