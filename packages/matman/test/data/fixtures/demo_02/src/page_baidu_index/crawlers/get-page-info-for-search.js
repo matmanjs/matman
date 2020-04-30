14fcd88e48c7fc72e9d9124be62f5c7bb3b7f800
linjianghe <linjianghe@tencent.com>
2020-04-30 16:12:01 +0800

    feat: add demo_02

---
+++ packages/matman/test/data/fixtures/demo_02/src/page_baidu_index/crawlers/get-page-info-for-search.js
@@ -0,0 +1,42 @@
+module.exports = () => {
+    return {
+        title: document.title,
+        searchInputInfo: getSearchInputInfo(),
+        searchResultInfo: getSearchResultInfo()
+    };
+};
+
+/**
+ * 获取搜索框相关的信息
+ */
+function getSearchInputInfo() {
+    return {
+        keyWorld: jQuery('#kw').val(),
+        searchBtnText: jQuery('#su').val()
+    };
+}
+
+/**
+ * 获取搜索结果相关的信息
+ */
+function getSearchResultInfo() {
+    const jqContainer = jQuery('#content_left');
+    const result = {
+        isExist: !!jqContainer.length,
+        list: []
+    };
+
+    function getItemData(jqItem) {
+        return {
+            title: jQuery('.t', jqItem).text().trim(),
+            describe: jQuery('.c-abstract', jqItem).text().trim(),
+            tpl: jqItem.attr('tpl')
+        };
+    }
+
+    jQuery('.c-container', jqContainer).each(function () {
+        result.list.push(getItemData(jQuery(this)));
+    });
+
+    return result;
+}
