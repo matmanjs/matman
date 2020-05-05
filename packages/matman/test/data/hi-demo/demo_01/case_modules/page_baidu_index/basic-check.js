a2567bef283f93da9121562cc4e54e38ebe4dbfe
helinjiang <onehlj@qq.com>
2020-05-05 13:03:12 +0800

    fix: setDeviceConfig 补充 name

--- packages/matman/test/data/hi-demo/demo_01/case_modules/page_baidu_index/basic-check.js
+++ packages/matman/test/data/hi-demo/demo_01/case_modules/page_baidu_index/basic-check.js
@@ -14,6 +14,7 @@ function getResult(opts) {
 
         // 设置浏览器参数
         .setDeviceConfig({
+            'name': 'mydevice',
             'UA': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36 mycustomua',
             'width': 1250,
             'height': 400
