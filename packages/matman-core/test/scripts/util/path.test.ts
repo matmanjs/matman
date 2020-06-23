3dfb5de3aa9a5bc874da2e1087168254a857d4a5
helinjiang <onehlj@qq.com>
2020-06-23 22:17:28 +0800

    feat: 补充单元测试

--- packages/matman-core/test/scripts/util/path.test.ts
+++ packages/matman-core/test/scripts/util/path.test.ts
@@ -1,7 +1,11 @@
 import 'mocha';
 import {expect} from 'chai';
 
-import {getNewFilePathWithTag, getFolderNameFromPath} from '../../../src/util/path';
+import {
+  getNewFilePathWithTag,
+  getFolderNameFromPath,
+  getSaveDirFromPath,
+} from '../../../src/util/path';
 
 describe('./util/path.ts', function () {
   describe('check getNewFilePathWithTag(filePath, tag)', function () {
@@ -107,6 +111,58 @@ describe('./util/path.ts', function () {
 
     it('D:\\root\\a\\b\\c.min.js -> D_root_a_b_c_min_js', function () {
       expect(getFolderNameFromPath('D:\\root\\a\\b\\c.min.js')).to.equal('D_root_a_b_c_min_js');
+    });
+  });
+
+  describe('check getSaveDirFromPath(targetPath)', function () {
+    it('a/b/c.min.js -> a/b/c.min.js', function () {
+      expect(getSaveDirFromPath('a/b/c.min.js')).to.equal('a/b/c_min_js');
+    });
+
+    it('./a/b/c.min.js -> a_b_c_min_js', function () {
+      expect(getSaveDirFromPath('./a/b/c.min.js')).to.equal('a/b/c_min_js');
+    });
+
+    it('../a/b/c.min.js -> parent_a_b_c_min_js', function () {
+      expect(getSaveDirFromPath('../a/b/c.min.js')).to.equal('parent/a/b/c_min_js');
+    });
+
+    it('../../a/b/c.min.js -> parent_parent_a_b_c_min_js', function () {
+      expect(getSaveDirFromPath('../../a/b/c.min.js')).to.equal('parent/parent/a/b/c_min_js');
+    });
+
+    it('/root/a/b/c.min.js -> root_root_a_b_c_min_js', function () {
+      expect(getSaveDirFromPath('/root/a/b/c.min.js')).to.equal('root/root/a/b/c_min_js');
+    });
+
+    it('c.min.js -> c_min_js', function () {
+      expect(getSaveDirFromPath('c.min.js')).to.equal('c_min_js');
+    });
+
+    it('iamfile -> iamfile', function () {
+      expect(getSaveDirFromPath('iamfile')).to.equal('iamfile');
+    });
+
+    it('a\\b\\c.min.js -> a_b_c_min_js', function () {
+      expect(getSaveDirFromPath('a\\b\\c.min.js')).to.equal('a\\b\\c_min_js');
+    });
+
+    it('.\\a\\b\\c.min.js -> a_b_c_min_js', function () {
+      expect(getSaveDirFromPath('.\\a\\b\\c.min.js')).to.equal('a\\b\\c_min_js');
+    });
+
+    it('..\\a\\b\\c.min.js -> parent_a_b_c_min_js', function () {
+      expect(getSaveDirFromPath('..\\a\\b\\c.min.js')).to.equal('parent\\a\\b\\c_min_js');
+    });
+
+    it('..\\..\\a\\b\\c.min.js -> parent_parent_a_b_c_min_js', function () {
+      expect(getSaveDirFromPath('..\\..\\a\\b\\c.min.js')).to.equal(
+        'parent\\parent\\a\\b\\c_min_js',
+      );
+    });
+
+    it('D:\\root\\a\\b\\c.min.js -> D_root_a_b_c_min_js', function () {
+      expect(getSaveDirFromPath('D:\\root\\a\\b\\c.min.js')).to.equal('D\\root\\a\\b\\c_min_js');
     });
   });
 });
