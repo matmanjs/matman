b6c5f7b5a87714de0a1e6931eeac3e8318d913c4
helinjiang <onehlj@qq.com>
2020-06-24 10:28:21 +0800

    feat: add unit test

--- packages/matman-crawler/test/scripts/run-build/index.test.ts
+++ packages/matman-crawler/test/scripts/run-build/index.test.ts
@@ -10,43 +10,108 @@ describe('check run-build/index.ts', () => {
   const rootPath = path.join(__dirname, '../../data/fixtures/demo_for_crawlers');
   const tmpCrawlerBuildPath = path.join(__dirname, '../../data/tmp/demo_for_crawlers_build');
 
-  const matmanConfig = findMatmanConfig(rootPath, {
-    crawlerBuildPath: tmpCrawlerBuildPath,
-  }) as MatmanConfig;
+  describe('check isIPC', () => {
+    const matmanConfig = findMatmanConfig(rootPath, {
+      crawlerBuildPath: tmpCrawlerBuildPath,
+    }) as MatmanConfig;
 
-  before(() => {
-    fse.removeSync(tmpCrawlerBuildPath);
-  });
+    it('build simple file when isIPC is true', () => {
+      const entryPath = path.join(
+        __dirname,
+        '../../data/fixtures/demo_for_crawlers/case_modules/crawlers/simple-file.js',
+      );
+      const outputPath = path.join(
+        __dirname,
+        '../../data/expects/demo_for_crawlers/simple-file-isIPC.js.result',
+      );
+      return build(entryPath, {isIPC: true, matmanConfig}).then(result => {
+        expect(result).to.equal(fse.readFileSync(outputPath, {encoding: 'utf8'}));
+      });
+    });
 
-  after(() => {
-    fse.removeSync(tmpCrawlerBuildPath);
-  });
-
-  it('build simple file when isIPC is true', () => {
-    const entryPath = path.join(
-      __dirname,
-      '../../data/fixtures/demo_for_crawlers/case_modules/crawlers/simple-file.js',
-    );
-    const outputPath = path.join(
-      __dirname,
-      '../../data/expects/demo_for_crawlers/simple-file-isIPC.js.result',
-    );
-    return build(entryPath, {isIPC: true, matmanConfig}).then(result => {
-      expect(result).to.equal(fse.readFileSync(outputPath, {encoding: 'utf8'}));
+    it('build simple file when isIPC is false', () => {
+      const entryPath = path.join(
+        __dirname,
+        '../../data/fixtures/demo_for_crawlers/case_modules/crawlers/simple-file.js',
+      );
+      const outputPath = path.join(
+        __dirname,
+        '../../data/expects/demo_for_crawlers/simple-file.js.result',
+      );
+      return build(entryPath, {isIPC: false, matmanConfig}).then(result => {
+        expect(result).to.equal(fse.readFileSync(outputPath, {encoding: 'utf8'}));
+      });
     });
   });
 
-  it('build simple file when isIPC is false', () => {
+  describe('check matmanConfig', () => {
     const entryPath = path.join(
       __dirname,
       '../../data/fixtures/demo_for_crawlers/case_modules/crawlers/simple-file.js',
     );
-    const outputPath = path.join(
-      __dirname,
-      '../../data/expects/demo_for_crawlers/simple-file.js.result',
-    );
-    return build(entryPath, {isIPC: false, matmanConfig}).then(result => {
-      expect(result).to.equal(fse.readFileSync(outputPath, {encoding: 'utf8'}));
+
+    it('isDevBuild=false and crawlerInjectJQuery=false', () => {
+      const matmanConfig = findMatmanConfig(rootPath, {
+        crawlerBuildPath: tmpCrawlerBuildPath,
+        isDevBuild: false,
+        crawlerInjectJQuery: false,
+      }) as MatmanConfig;
+
+      const outputPath = path.join(
+        __dirname,
+        '../../data/expects/demo_for_crawlers/matman-config-not-dev-no-jquery.js.result',
+      );
+      return build(entryPath, {isIPC: false, matmanConfig}).then(result => {
+        expect(result).to.equal(fse.readFileSync(outputPath, {encoding: 'utf8'}));
+      });
+    });
+
+    it('isDevBuild=false and crawlerInjectJQuery=true', () => {
+      const matmanConfig = findMatmanConfig(rootPath, {
+        crawlerBuildPath: tmpCrawlerBuildPath,
+        isDevBuild: false,
+        crawlerInjectJQuery: true,
+      }) as MatmanConfig;
+
+      const outputPath = path.join(
+        __dirname,
+        '../../data/expects/demo_for_crawlers/matman-config-not-dev-need-jquery.js.result',
+      );
+      return build(entryPath, {isIPC: false, matmanConfig}).then(result => {
+        expect(result).to.equal(fse.readFileSync(outputPath, {encoding: 'utf8'}));
+      });
+    });
+
+    it('isDevBuild=true and crawlerInjectJQuery=false', () => {
+      const matmanConfig = findMatmanConfig(rootPath, {
+        crawlerBuildPath: tmpCrawlerBuildPath,
+        isDevBuild: true,
+        crawlerInjectJQuery: false,
+      }) as MatmanConfig;
+
+      const outputPath = path.join(
+        __dirname,
+        '../../data/expects/demo_for_crawlers/matman-config-is-dev-no-jquery.js.result',
+      );
+      return build(entryPath, {isIPC: false, matmanConfig}).then(result => {
+        expect(result).to.equal(fse.readFileSync(outputPath, {encoding: 'utf8'}));
+      });
+    });
+
+    it('isDevBuild=true and crawlerInjectJQuery=true', () => {
+      const matmanConfig = findMatmanConfig(rootPath, {
+        crawlerBuildPath: tmpCrawlerBuildPath,
+        isDevBuild: true,
+        crawlerInjectJQuery: true,
+      }) as MatmanConfig;
+
+      const outputPath = path.join(
+        __dirname,
+        '../../data/expects/demo_for_crawlers/matman-config-is-dev-need-jquery.js.result',
+      );
+      return build(entryPath, {isIPC: false, matmanConfig}).then(result => {
+        expect(result).to.equal(fse.readFileSync(outputPath, {encoding: 'utf8'}));
+      });
     });
   });
 });
