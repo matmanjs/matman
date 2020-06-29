e94eae5146fa35d88da8de8135f2c3a842298185
helinjiang <onehlj@qq.com>
2020-06-29 18:07:56 +0800

    refactor: network 的请求问题

---
+++ demo/sample_network/nightmare-baidu.js
@@ -0,0 +1,33 @@
+const matman = require('../../packages/matman');
+const {BrowserRunner} = require('../../packages/matman-runner-nightmare');
+
+module.exports = async pageDriverOpts => {
+  const pageDriver = await matman.launch(new BrowserRunner(), pageDriverOpts);
+
+  await pageDriver.setScreenshotConfig(true);
+
+  await pageDriver.setPageUrl('https://www.baidu.com');
+
+  await pageDriver.wait('#su');
+
+  return await pageDriver.evaluate(() => {
+    return {
+      title: document.title,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  });
+};
+
+// module
+//   .exports({
+//     show: true,
+//     doNotCloseBrowser: true,
+//     useRecorder: true,
+//   })
+//   .then(function (result) {
+//     console.log(JSON.stringify(result));
+//   })
+//   .catch(function (error) {
+//     console.error('failed:', error);
+//   });
