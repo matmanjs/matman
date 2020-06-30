8db6a7f56ff3699c995eb477857235b8b44da816
derekjqwang <derekjqwang@tencent.com>
2020-06-30 14:40:02 +0800

    feat: 更新cli中的模板

--- packages/matman-cli/generators/basic/templates/case_modules/page_baidu_index/basic-check.matman.copy.js
+++ packages/matman-cli/generators/basic/templates/case_modules/page_baidu_index/basic-check.matman.copy.js
@@ -1,33 +1,34 @@
-const matman = require('matman');
-const {PuppeteerMaster} = require('matman-runner-puppeteer');
+const matman = require("matman");
+const { BrowserRunner } = require("matman-runner-puppeteer");
 
 module.exports = async () => {
-  const page = await new matman.launch(PuppeteerMaster, {
+  const page = matman.launch(new BrowserRunner(), {
     show: true,
     doNotCloseBrowser: true,
     useRecorder: false,
-  }).newPage(__filename);
+  });
 
   await page.setDeviceConfig({
     UA:
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36 mycustomua',
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36 mycustomua",
     width: 1250,
     height: 400,
   });
   await page.setScreenshotConfig(true);
-  await page.goto('https://www.baidu.com');
-  await page.wait('#su');
-  await page.evaluate(() => {
+  await page.setPageUrl("https://www.baidu.com");
+  await page.addAction("SCAN_PAGE", async (page) => {
+    await page.waitFor("#su");
+  });
+  const res = await page.evaluate(() => {
     return {
       title: document.title,
       width: window.innerWidth,
       height: window.innerHeight,
       userAgent: navigator.userAgent,
       _version: Date.now(),
-      searchBtnTxt: document.querySelector('#su').value,
+      searchBtnTxt: document.querySelector("#su").value,
     };
   });
-  const res = await page.end();
   return res;
 };
 
@@ -37,5 +38,5 @@ module
     console.log(JSON.stringify(result));
   })
   .catch(function (error) {
-    console.error('failed:', error);
+    console.error("failed:", error);
   });
