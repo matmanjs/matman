156235ce79a0b482338f8a3e6e92f38dcb9088cb
derekjqwang <derekjqwang@tencent.com>
2020-07-14 15:25:11 +0800

    test: 修正测试用例

--- packages/matman-core/src/config/ScreenshotConfig.ts
+++ packages/matman-core/src/config/ScreenshotConfig.ts
@@ -74,7 +74,7 @@ export default class ScreenshotConfig {
       // 截图的区域，例如 { x: 0, y: 0, width: 0, height: 0 }
       // https://github.com/electron/electron/blob/master/docs/api/browser-window.md#winsetthumbnailclipregion-windows
       this.clip = opts.clip;
-      this.fullPage = opts.fullPage ? opts.fullPage : true;
+      this.fullPage = opts.fullPage === false ? false : true;
     } else if (typeof opts === 'string') {
       // 如果 opts 为字符串，则代表设置的是截图保存路径
       this.path = this.getScreenshotFullPath(opts, matmanConfig.screenshotPath);
