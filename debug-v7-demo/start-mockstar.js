ccf81b2c36a4db55906584fc171c762ed779552f
helinjiang <onehlj@qq.com>
2021-05-08 13:42:26 +0800

    feat: plugin mockstar

---
+++ debug-v7-demo/start-mockstar.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const mockstarLocalServer = require('mockstar-local-server');
+
+// 服务启动参数
+const configOpts = {
+  rootPath: __dirname,
+  mockServerPath: path.resolve(__dirname, './matman-app/src/plugins/mockstar/modules'),
+};
+
+// 启动本地服务
+const runServer = mockstarLocalServer.startServer(configOpts, (isSuccess, data) => {
+  console.log('startServer callback', isSuccess, data);
+});
+
+// 3s 之后停止服务
+// setTimeout(() => {
+//     runServer.stop(() => {
+//         console.log('stop server success!');
+//     });
+// }, 3000);
+
+// 使用该方式，可以直接启动一个mockstar，而不一定需要配置 mockstar.config.js
--- packages/matman-plugin-app/src/PluginApp.ts
+++ packages/matman-plugin-app/src/PluginApp.ts
@@ -30,20 +30,6 @@ export default class PluginApp extends PluginBase {
    * 构建业务工程项目
    */
   public async build(cmd: IBuildAppCmd, opts?: IBuildAppOpts) {
-    // TODO 该值从 setting.json 中的 plugins.app.activeInstance 获取对应的配置读取
-    // const cmd = (port?: number) => `npx cross-env ENABLE_E2E_TEST=1 PORT=${port} npm start`;
-
-    // // TODO 读取配置
-    // const rootPath = path.resolve('/Users/helinjiang/gitprojects/matman-v7-demo');
-
-    // // TODO 读取配置
-    // const opts = {
-    //   cwd: rootPath,
-    //   port: process.env.PROJECT_PORT,
-    //   usePort: true,
-    //   checkIfBuildCompleted: (stdoutData: string) => !!(stdoutData && stdoutData.indexOf('Compiled successfully') > -1),
-    // } ;
-
     console.log('--PluginApp build--', this);
 
     return buildApp(cmd, opts);
