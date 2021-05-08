384c6bcd4e359cfc65a3beb3ac52315ca2713d19
helinjiang <onehlj@qq.com>
2021-05-08 10:08:26 +0800

    feat: plugin whistle

--- packages/matman-plugin-whistle/src/PluginWhistle.ts
+++ packages/matman-plugin-whistle/src/PluginWhistle.ts
@@ -1,8 +1,12 @@
 // import path from 'path';
 import { PluginBase } from 'matman-plugin-core';
 
-import { startWhistle } from './utils';
+import WhistleSDK from 'whistle-sdk';
 
+
+interface IPluginWhistleOpts {
+  port?: number
+}
 
 export default class PluginWhistle extends PluginBase {
   /**
@@ -10,23 +14,28 @@ export default class PluginWhistle extends PluginBase {
    */
   public seqId: string;
 
+  private readonly whistleSDK: WhistleSDK;
 
-  public constructor() {
+  public constructor(opts?: IPluginWhistleOpts) {
     super('whistle');
 
     this.seqId = `${this.name}-${Math.random()}`;
+
+    this.whistleSDK = new WhistleSDK({
+      seqId: this.seqId,
+      port: parseInt(`${process.env.MATMAN_PLUGIN_WHISTLE_PORT || opts?.port || 0}`, 10),
+      forceOverride: true,
+      useCurrentStartedWhistle: false,
+    });
   }
 
   public setSeqId(seqId: string) {
     this.seqId = seqId;
   }
 
-
   public async setup() {
     console.log('==whistle== setup');
 
-    await startWhistle({
-      seqId: this.seqId,
-    });
+    await this.whistleSDK.start();
   }
 }
