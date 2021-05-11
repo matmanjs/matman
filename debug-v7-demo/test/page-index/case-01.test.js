0302cb93c25bb407d5b21b1628431f25fa617eb3
helinjiang <onehlj@qq.com>
2021-05-11 19:46:26 +0800

    feat: 引入 test

---
+++ debug-v7-demo/test/page-index/case-01.test.js
@@ -0,0 +1,51 @@
+const { expect } = require('chai');
+
+const checkPage = require('../../matman-app/src/case_modules/page-index/case-01');
+
+describe('index.html 身份证认证页面：常规检查', function () {
+  this.timeout(30000);
+
+  let matmanResult;
+
+  before(async function () {
+    matmanResult = await checkPage({
+      show: false,
+      doNotCloseBrowser: false,
+      useRecorder: true
+    });
+  });
+
+  describe('检查基本信息', function () {
+    let data;
+
+    before(function () {
+      data = matmanResult.get('init');
+    });
+
+    it('数据快照校验通过', function () {
+      expect(data).to.eql({
+        formBtnInfo: {
+          isBtnActive: false,
+          isExist: true,
+          text: '申请验证'
+        },
+        formContentInfo: {
+          idValue: '',
+          idValueInputType: 'tel',
+          idValueLabel: '身份证',
+          idValuePlaceholder: '请输入身份证',
+          isExist: true,
+          nameValue: '',
+          nameValueInputType: 'text',
+          nameValueLabel: '姓名',
+          nameValuePlaceholder: '请输入姓名'
+        },
+        messageTipsInfo: { isExist: false },
+        titleInfo: {
+          isExist: true,
+          title: '请核实身份信息，确保成功提现'
+        }
+      });
+    });
+  });
+});
