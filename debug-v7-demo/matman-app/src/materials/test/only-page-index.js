29c6a38da09bfb1c3bfa52756c247476977cda94
helinjiang <onehlj@qq.com>
2021-06-03 21:32:35 +0800

    feat: add another test

---
+++ debug-v7-demo/matman-app/src/materials/test/only-page-index.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const { PluginTestMochaMaterial } = require('../../../../../packages/matman-plugin-test');
+
+// module.exports = new MochaInstance('npm run test:e2e:direct', {
+//   // cwd: e2eRunner.workspacePath,
+//   // whistlePort,
+//   // matmanAppPath,
+//   // mochawesomeJsonFilePath: path.join(e2eRunner.outputPath, './mochawesome/mochawesome.json'),
+// });
+
+module.exports = new PluginTestMochaMaterial('只测试 page-index', 'npx mocha test/page-index', {
+  cwd: path.join(__dirname, '../../../../'),
+});
