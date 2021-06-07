bb206ba9a49a948878300e65d151281365fc769f
helinjiang <onehlj@qq.com>
2021-06-07 17:20:48 +0800

    refactor: 修复一个变量问题

--- debug-v7-demo/matman-app/src/case_modules/page-index/materials/mock-services/basic.js
+++ debug-v7-demo/matman-app/src/case_modules/page-index/materials/mock-services/basic.js
@@ -1,6 +1,6 @@
-const { PluginMockstarInstance } = require('../../../../../../../packages/matman');
+const { PluginMockstarMaterial } = require('../../../../../../../packages/matman');
 
-module.exports = new PluginMockstarInstance('身份证和手机都校验成功', {
+module.exports = new PluginMockstarMaterial('身份证和手机都校验成功', {
   queryMap: {
     // 校验身份证
     'verify-identity': 'success',
--- debug-v7-demo/matman-app/src/case_modules/page-withdraw/materials/mock-services/basic.js
+++ debug-v7-demo/matman-app/src/case_modules/page-withdraw/materials/mock-services/basic.js
@@ -1,6 +1,6 @@
-const { PluginMockstarInstance } = require('../../../../../../../packages/matman');
+const { PluginMockstarMaterial } = require('../../../../../../../packages/matman');
 
-module.exports = new PluginMockstarInstance('存在金额', {
+module.exports = new PluginMockstarMaterial('存在金额', {
   queryMap: {
     // 获取金额
     'get-money': 'success',
