132b39f1e1dba0f0ed8ee29ffc544decd04ff837
helinjiang <onehlj@qq.com>
2021-06-08 22:37:19 +0800

    refactor: 将 app material 修改为直接导出，放弃使用 function

--- debug-v7-demo/matman-app/src/materials/app/dev.js
+++ debug-v7-demo/matman-app/src/materials/app/dev.js
@@ -1,27 +1,26 @@
 const path = require('path');
 const { PluginAppMaterial } = require('../../../../../packages/matman');
 
-module.exports = () => {
-  // app 项目根目录
-  const rootPath = path.resolve(__dirname, '../../../../../test/data/project-react-h5');
+// app 项目根目录
+const rootPath = path.resolve(__dirname, '../../../../../test/data/project-react-h5');
 
-  return new PluginAppMaterial('构建本地开发版本', {
-    rootPath,
-    setup: async pluginApp => {
-      await pluginApp.build(port => `npx cross-env ENABLE_E2E_TEST=1 PORT=${port} npm start`, {
-        cwd: rootPath,
-        port: process.env.PROJECT_PORT,
-        usePort: true,
-        checkIfBuildCompleted: stdoutData =>
-          stdoutData && stdoutData.indexOf('Compiled successfully') > -1,
-      });
-    },
-    getWhistleRules: port => {
-      return [
-        'now.qq.com/maybe/report statusCode://200',
-        `now.qq.com 127.0.0.1:${port}`,
-        `now.qq.com/manifest.json ${rootPath}/public/manifest.json`,
-      ];
-    },
-  });
-};
+module.exports = new PluginAppMaterial('构建本地开发版本', {
+  rootPath,
+  setup: async pluginApp => {
+    await pluginApp.build(port => `npx cross-env ENABLE_E2E_TEST=1 PORT=${port} npm start`, {
+      cwd: rootPath,
+      port: process.env.PROJECT_PORT,
+      usePort: true,
+      checkIfBuildCompleted: stdoutData =>
+        stdoutData && stdoutData.indexOf('Compiled successfully') > -1,
+    });
+  },
+  getWhistleRules: port => {
+    return [
+      'now.qq.com/maybe/report statusCode://200',
+      `now.qq.com 127.0.0.1:${port}`,
+      `now.qq.com/manifest.json ${rootPath}/public/manifest.json`,
+    ];
+  },
+});
+
--- debug-v7-demo/matman-app/src/materials/app/prod.js
+++ debug-v7-demo/matman-app/src/materials/app/prod.js
@@ -1,24 +1,22 @@
 const path = require('path');
 const { PluginAppMaterial } = require('../../../../../packages/matman');
 
-module.exports = () => {
-  // app 项目根目录
-  const rootPath = path.resolve(__dirname, '../../../../../test/data/project-react-h5');
+// app 项目根目录
+const rootPath = path.resolve(__dirname, '../../../../../test/data/project-react-h5');
 
-  return new PluginAppMaterial('构建生产环境版本', {
-    rootPath,
-    setup: async pluginApp => {
-      await pluginApp.build('npx cross-env ENABLE_E2E_TEST=1 npm run build', {
-        cwd: rootPath,
-      });
-    },
-    getWhistleRules: () => {
-      return [
-        'now.qq.com/maybe/report statusCode://200',
-        `now.qq.com/manifest.json ${rootPath}/build/manifest.json`,
-        `/^https?://now\\.qq\\.com/static/(.*)$/ ${rootPath}/build/static/$1`,
-        `/^https?://now\\.qq\\.com/([\\w\\-]*)(.*)$/ ${rootPath}/build/$1$2`,
-      ];
-    },
-  });
-};
+module.exports = new PluginAppMaterial('构建生产环境版本', {
+  rootPath,
+  setup: async pluginApp => {
+    await pluginApp.build('npx cross-env ENABLE_E2E_TEST=1 npm run build', {
+      cwd: rootPath,
+    });
+  },
+  getWhistleRules: () => {
+    return [
+      'now.qq.com/maybe/report statusCode://200',
+      `now.qq.com/manifest.json ${rootPath}/build/manifest.json`,
+      `/^https?://now\\.qq\\.com/static/(.*)$/ ${rootPath}/build/static/$1`,
+      `/^https?://now\\.qq\\.com/([\\w\\-]*)(.*)$/ ${rootPath}/build/$1$2`,
+    ];
+  },
+});
